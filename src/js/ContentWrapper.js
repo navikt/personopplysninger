d7f9d74de95148bd799d64c53292f92ec4983b5f
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-08-17 15:19:36 +0200

    Add Personalia information

--- src/js/ContentWrapper.js
+++ src/js/ContentWrapper.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Personalia from './components/Personalia';
 import Adresse from './components/Adresse';
 import Familierelasjoner from './components/Familierelasjoner';
@@ -8,7 +9,18 @@ class ContentWrapper extends Component {
   render() {
     return (
       <div>
-        <Personalia />
+        <Personalia
+          navn={this.props.userInfo.navn}
+          ident={this.props.userInfo.ident}
+          statsborgerskap={this.props.userInfo.statsborgerskap}
+          status={this.props.userInfo.status}
+          telefon={this.props.userInfo.telefon}
+          tiltak={this.props.userInfo.tiltak}
+          kjonn={this.props.userInfo.kjonn}
+          spraak={this.props.userInfo.spraak}
+          sivilstand={this.props.userInfo.sivilstand}
+          kontonummer={this.props.userInfo.kontonummer}
+        />
         <Adresse />
         <Familierelasjoner />
         <Arbeidsforhold />
@@ -18,3 +30,7 @@ class ContentWrapper extends Component {
 }
 
 export default ContentWrapper;
+
+ContentWrapper.propTypes = {
+  userInfo: PropTypes.shape.isRequired,
+};
--- src/js/components/Personalia.js
+++ src/js/components/Personalia.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Box from 'js/components/Box';
 
 class Personalia extends Component {
@@ -6,9 +7,88 @@ class Personalia extends Component {
     return (
       <Box>
         <h3>Personalia</h3>
+        <ul className="personalia-list">
+          <li>
+            <span className="title">Fornavn</span>
+            <span className="content">{this.props.navn.fornavn}</span>
+          </li>
+          <li>
+            <span className="title">Etternavn</span>
+            <span className="content">{this.props.navn.slektsnavn}</span>
+          </li>
+          <li>
+            <span className="title">Fødselsnummer</span>
+            <span className="content">{this.props.ident}</span>
+          </li>
+          <li>
+            <span className="title">Utenlandsk ID</span>
+            <span className="content">x</span>
+          </li>
+          <li>
+            <span className="title">Telefon</span>
+            <span className="content">{this.props.telefon.privat}</span>
+          </li>
+          <li>
+            <span className="title">Kontonummer</span>
+            <span className="content">{this.props.kontonummer.nummer}</span>
+          </li>
+          <li>
+            <span className="title">e-post</span>
+            <span className="content">x</span>
+          </li>
+          <li>
+            <span className="title">Språk</span>
+            <span className="content">{this.props.spraak.kode}</span>
+          </li>
+          <li>
+            <span className="title">Statsborgerskap</span>
+            <span className="content">{this.props.statsborgerskap.kode}</span>
+          </li>
+          <li>
+            <span className="title">Personstatus</span>
+            <span className="content">{this.props.status.kode}</span>
+          </li>
+          <li>
+            <span className="title">Fødested</span>
+            <span className="content">x</span>
+          </li>
+          <li>
+            <span className="title">Spesialregistrering</span>
+            <span className="content">x</span>
+          </li>
+          <li>
+            <span className="title">Sivilstand</span>
+            <span className="content">{this.props.sivilstand.kode}</span>
+          </li>
+          <li>
+            <span className="title">Sikkerhetstiltak</span>
+            <span className="content">{this.props.tiltak.type}</span>
+          </li>
+          <li>
+            <span className="title">Kjønn</span>
+            <span className="content">{this.props.kjonn}</span>
+          </li>
+          <li>
+            <span className="title">Tilpasset kommunikasjon</span>
+            <span className="content">x</span>
+          </li>
+        </ul>
       </Box>
     );
   }
 }
+
+Personalia.propTypes = {
+  navn: PropTypes.shape.isRequired,
+  ident: PropTypes.string.isRequired,
+  statsborgerskap: PropTypes.shape.isRequired,
+  status: PropTypes.shape.isRequired,
+  telefon: PropTypes.shape.isRequired,
+  tiltak: PropTypes.shape.isRequired,
+  kjonn: PropTypes.string.isRequired,
+  spraak: PropTypes.shape.isRequired,
+  sivilstand: PropTypes.shape.isRequired,
+  kontonummer: PropTypes.shape.isRequired,
+};
 
 export default Personalia;
