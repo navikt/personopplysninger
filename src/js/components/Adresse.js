595816fe54c258a05a538314fa3ac3f60d4d8468
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-08-30 16:33:29 +0200

    Add bostedsadresse

--- src/js/components/Adresse.js
+++ src/js/components/Adresse.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import Box from 'js/components/Box';
+import woman from '../../assets/img/woman.png';
 
 class Adresse extends Component {
   render() {
     return (
-      <Box header="Adresse">
-        <h3>Adresse</h3>
+      <Box header="Adresse" icon={woman}>
+        <h3 className="address-type">Bostedsadresse</h3>
+        <ul className="address-row">
+          <li className="address-col">
+            <span className="title">Gate</span>
+            <span className="content">Vardeveien 7</span>
+          </li>
+          <li className="address-col">
+            <span className="title">Postnummer</span>
+            <span className="content">5002</span>
+          </li>
+          <li className="address-col">
+            <span className="title">Poststed</span>
+            <span className="content">Kristiansand</span>
+          </li>
+        </ul>
+        <div className="box-footer">
+          Kilde: Folkeregisteret
+        </div>
       </Box>
     );
   }
