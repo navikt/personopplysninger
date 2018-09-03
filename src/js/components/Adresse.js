61f75556dde2d2f213630ddc439554d151d8b85d
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-09-03 09:05:22 +0200

    Style address box

--- src/js/components/Adresse.js
+++ src/js/components/Adresse.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import Box from 'js/components/Box';
-import woman from '../../assets/img/woman.png';
+import { FormattedMessage } from 'react-intl';
+import house from '../../assets/img/house.png';
 
 class Adresse extends Component {
   render() {
     return (
-      <Box header="Adresse" icon={woman}>
-        <h3 className="address-type">Bostedsadresse</h3>
-        <ul className="address-row">
-          <li className="address-col">
-            <span className="title">Gate</span>
-            <span className="content">Vardeveien 7</span>
-          </li>
-          <li className="address-col">
-            <span className="title">Postnummer</span>
-            <span className="content">5002</span>
-          </li>
-          <li className="address-col">
-            <span className="title">Poststed</span>
-            <span className="content">Kristiansand</span>
-          </li>
-        </ul>
-        <div className="box-footer">
-          Kilde: Folkeregisteret
+      <Box header="Adresse" icon={house}>
+        <div className="address-box">
+          <h3 className="address-type">Bostedsadresse</h3>
+          <ul className="address-list">
+            <li className="address-col">
+              <span className="title"><FormattedMessage
+                id="adresse.gate"
+              />
+              </span>
+              <span className="content">Vardeveien 7</span>
+            </li>
+            <li className="address-col">
+              <span className="title"><FormattedMessage
+                id="adresse.postnummer"
+              />
+              </span>
+              <span className="content">5002</span>
+            </li>
+            <li className="address-col">
+              <span className="title"><FormattedMessage
+                id="adresse.poststed"
+              />
+              </span>
+              <span className="content">Kristiansand</span>
+            </li>
+          </ul>
+          <div className="box-footer">
+            Kilde: Folkeregisteret
+          </div>
         </div>
       </Box>
     );
