a0f51f4649409708a9f26a061207aed9f82cc53d
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-09-13 14:40:47 +0200

    Fix formatting

--- src/js/containers/LinksContainer.js
+++ src/js/containers/LinksContainer.js
@@ -1,35 +1,23 @@
-/* eslint-disable */
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import 'less/index.less';
 import links from '../links';
 import LinkBox from '../components/LinkBox';
 
 class LinksContainer extends Component {
-
   render() {
-
-    console.log('links');
-    console.log(links);
-
     return (
       <React.Fragment>
         {links.map(link =>
-          <LinkBox
+          (<LinkBox
             header={link.header}
             information={link.information}
             url={link.url}
             linkText={link.linkText}
             kilde={link.kilde}
-          />
-        )}
+          />))}
       </React.Fragment>
     );
   }
 }
-
-LinksContainer.propTypes = {
-  links: PropTypes.shape({}).isRequired,
-};
 
 export default LinksContainer;
