f8ed025f2461ecfadc7dadb08598e522fcc078d2
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-09-14 10:32:26 +0200

    Refactor by adding a ListElement component

--- src/js/components/adresse/UtenlandskAdresse.js
+++ src/js/components/adresse/UtenlandskAdresse.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Box from 'js/components/Box';
-import { FormattedMessage } from 'react-intl';
+import ListElement from '../ListElement';
 
 class UtenlandskAdresse extends Component {
   render() {
@@ -10,27 +10,21 @@ class UtenlandskAdresse extends Component {
         <div className="address-box">
           <h3 className="address-type">Utenlandsk adresse</h3>
           <ul className="address-list-col-3">
-            <li className="address-col">
-              <span className="title"><FormattedMessage
-                id="adresse.adresse1"
-              />
-              </span>
-              <span className="content">{this.props.adresse1}</span>
-            </li>
-            <li className="address-col">
-              <span className="title"><FormattedMessage
-                id="adresse.adresse2"
-              />
-              </span>
-              <span className="content">{this.props.adresse2}</span>
-            </li>
-            <li className="address-col">
-              <span className="title"><FormattedMessage
-                id="adresse.adresse3"
-              />
-              </span>
-              <span className="content">{this.props.adresse3}</span>
-            </li>
+            <ListElement
+              className="address-col"
+              titleId="adresse.adresse1"
+              content={this.props.adresse1}
+            />
+            <ListElement
+              className="address-col"
+              titleId="adresse.adresse2"
+              content={this.props.adresse2}
+            />
+            <ListElement
+              className="address-col"
+              titleId="adresse.adresse3"
+              content={this.props.adresse3}
+            />
           </ul>
           <div className="box-footer">
           Kilde: {this.props.kilde}
