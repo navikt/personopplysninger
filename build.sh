edd139854954f1c098031422f48dd36edebb2330
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-09-21 14:48:02 +0200

    Log

--- build.sh
+++ build.sh
@@ -48,8 +48,8 @@ done
         "$@"
 }
  function install_packages {
+    build_command cd /var/workspace
     build_command ls -la
-    build_command pwd
     build_command yarn
 }
  function build_frontend {
