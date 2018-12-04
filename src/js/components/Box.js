96b1023b2b3c82e2517b553e0adafe233779fb40
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-12-04 16:00:35 +0100

    Add logic for toggling info box

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import infoBox from '../../assets/img/infomation-circle.png';
 
 class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayInfo: false };
+  }
+
   render() {
     const marginClass = this.props.smallMargin ? 'box-bottom-margin-small' : 'box-bottom-margin';
+
+    const toggleInfo = () => {
+      this.setState({
+        displayInfo: !this.state.displayInfo,
+      });
+    };
 
     return (
       <div className={`BoxContainer ${marginClass}`}>
@@ -12,6 +24,10 @@ class Box extends Component {
           <div className="icon-box-wrapper">
             {this.props.icon ? <img src={this.props.icon} alt="" className="box-icon" /> : <div className="box-icon" />}
             <div className="Box">
+              <button className="information-circle" onClick={() => toggleInfo()}>
+                <img src={infoBox} alt="Information" />
+              </button>
+              {this.state.displayInfo ? <div>Info</div> : null}
               {this.props.children}
             </div>
           </div>
