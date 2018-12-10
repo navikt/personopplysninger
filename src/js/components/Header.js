a103c4143719174e7e4e2fbb4cba03159bf0cf9a
Asbjørn Steinskog <asbjorn@steinskog.me>
2018-12-10 10:14:39 +0100

    Some Header styling

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Snakkeboble from 'nav-frontend-snakkeboble';
+import pc from '../../assets/img/pc.png';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <h1 align="center">Dine personopplysninger</h1>
+        <h1 className="header-text">Dine personopplysninger</h1>
+        <Snakkeboble
+          pilHoyre
+        >
+          Heisann sveisann
+        </Snakkeboble>
+        <img src={pc} className="header-pc-icon" alt="Pc" />
       </div>
     );
   }
