1cced000df55ce33bc22b9fa1a72dbcd75eb7ae8
Sissel Fladby <sisfla@gmail.com>
2019-02-26 13:38:24 +0100

    Ta inn less-styling fra nav-frontend-moduler

--- src/js/static/infoContent.js
+++ src/js/static/infoContent.js
@@ -1,10 +1,10 @@
 export default {
   personalia: {
-    header: 'Informasjon om personalia',
+    header: 'Personalia',
     content: 'NAV må vite hvem du er og hvordan vi kan kontakte deg. Vi trenger også opplysninger for å kunne vurdere om du har krav på ytelser og tjenester fra NAV. For å kunne utbetale penger trenger vi kontonummeret ditt.',
   },
   adresse: {
-    header: 'Informasjon om adresse',
+    header: 'Adresse',
     content: 'NAV må vite hvilken adresse vi kan kontakte deg på. Adressen din forteller også hvilket NAV-kontor du hører til.',
   },
   dine_saker: {
