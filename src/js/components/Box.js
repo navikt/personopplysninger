4a09ee4ecd89b63fe6a7458976cbcf1a31f5c100
Sissel Fladby <sisfla@gmail.com>
2019-02-28 14:13:20 +0100

    Dynamisk endring mellom mobilvisning og desktop ved bruk av hooks!

--- src/js/components/Box.js
+++ src/js/components/Box.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Ingress, Systemtittel } from "nav-frontend-typografi";
 import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
 import Panel from "nav-frontend-paneler";
 import infoContent from "../static/infoContent";
 
-const Box = props => {
+function Box(props) {
   const { header, icon, infoType, children, id } = props;
-
-  const erMobil = window.innerWidth <= 420;
+  const [erMobil, setErMobil] = useState(window.innerWidth <= 420);
 
   const desktopVersjon = (
     <Panel className="box" id={id}>
@@ -43,8 +42,17 @@ const Box = props => {
     </div>
   );
 
+
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setErMobil(window.innerWidth <= 420));
+    return function cleanup() {
+      window.removeEventListener("resize", () => setErMobil(window.innerWidth <= 420));
+    };
+  });
+
   return <>{erMobil ? mobilVersjon : desktopVersjon}</>;
-};
+}
 
 Box.propTypes = {
   id: PropTypes.string.isRequired,
