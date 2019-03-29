c9dfccf7d66aa9bc57393d1abd2e419905f63d29
Morten Jansrud <morten@founder.no>
2019-03-29 10:42:20 +0100

    Fjern engelsk midlertidig

--- src/index.tsx
+++ src/index.tsx
@@ -4,25 +4,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { IntlProvider, addLocaleData } from "react-intl";
 import nb from "react-intl/locale-data/nb";
-import en from "react-intl/locale-data/en";
 import { setUpMock } from "./clients/apiMock";
 import App from "./App";
 import "css/index.css";
 
 import nbMessages from "./translations/nb.json";
-import enMessages from "./translations/en.json";
 
 interface Messages {
   [key: string]: { [key: string]: string };
 }
 
+const browserLanguage = "nb";
 const messages: Messages = {
-  nb: nbMessages,
-  en: enMessages
+  nb: nbMessages
 };
 
-const browserLanguage = navigator.language.split(/[-_]/)[0];
-addLocaleData([...nb, ...en]);
+addLocaleData([...nb]);
 
 if (process.env.NODE_ENV === "development") {
   setUpMock();
