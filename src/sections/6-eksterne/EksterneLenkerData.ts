f99fe16562cca50c0ef316a90d02cf22869ea9d6
Morten Jansrud <morten@founder.no>
2019-04-01 09:50:07 +0200

    Legg til lenke til ditt sykefravær. Avvent nytt ikon

--- src/sections/6-eksterne/EksterneLenkerData.ts
+++ src/sections/6-eksterne/EksterneLenkerData.ts
@@ -2,34 +2,34 @@ import papirstabel from "../../assets/img/papirstabel.svg";
 import penger from "../../assets/img/penger.svg";
 import jobbsok from "../../assets/img/jobbsok.svg";
 import sparegris from "../../assets/img/spare-pensjonsgris.svg";
+import Environment from "../../utils/Environments";
+
+const { tjenesteUrl } = Environment();
 
 export default [
-  {
-    id: "pensjonsopptjening",
-    tittel: "eksternelenker.pensjon.tittel",
-    beskrivelse: "eksternelenker.pensjon.beskrivelse",
-    lenkeTekst: "eksternelenker.pensjon.lenkeTekst",
-    url: "https://tjenester.nav.no/pselv/publisering/dinpensjon.jsf",
-    icon: sparegris,
-    visible: true
-  },
   {
     id: "dine-saker",
     tittel: "eksternelenker.dinesaker.tittel",
     beskrivelse: "eksternelenker.dinesaker.beskrivelse",
     lenkeTekst: "eksternelenker.dinesaker.lenkeTekst",
-    url: "https://tjenester.nav.no/saksoversikt/app",
-    icon: papirstabel,
-    visible: true
+    url: `${tjenesteUrl}/saksoversikt/app`,
+    icon: papirstabel
   },
   {
     id: "utbetalinger",
     tittel: "eksternelenker.utbetalinger.tittel",
     beskrivelse: "eksternelenker.utbetalinger.beskrivelse",
     lenkeTekst: "eksternelenker.utbetalinger.lenkeTekst",
-    url: "https://tjenester.nav.no/utbetalingsoversikt/",
-    icon: penger,
-    visible: true
+    url: `${tjenesteUrl}/utbetalingsoversikt`,
+    icon: penger
+  },
+  {
+    id: "sykefravaer",
+    tittel: "eksternelenker.sykefravaer.tittel",
+    beskrivelse: "eksternelenker.sykefravaer.beskrivelse",
+    lenkeTekst: "eksternelenker.sykefravaer.lenkeTekst",
+    url: `${tjenesteUrl}/sykefravaer`,
+    icon: jobbsok
   },
   {
     id: "cv",
@@ -37,7 +37,14 @@ export default [
     beskrivelse: "eksternelenker.cv.beskrivelse",
     lenkeTekst: "eksternelenker.cv.lenkeTekst",
     url: "https://arbeidsplassen.nav.no/minside",
-    icon: jobbsok,
-    visible: true
+    icon: jobbsok
+  },
+  {
+    id: "pensjonsopptjening",
+    tittel: "eksternelenker.pensjon.tittel",
+    beskrivelse: "eksternelenker.pensjon.beskrivelse",
+    lenkeTekst: "eksternelenker.pensjon.lenkeTekst",
+    url: `${tjenesteUrl}/pselv/publisering/dinpensjon.jsf`,
+    icon: sparegris
   }
 ];
