7a08d97d41b8ad680a040b906ecbd12847fe559f
Morten Jansrud <morten@founder.no>
2019-04-02 08:30:52 +0200

    Feil bruk av tegn

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,7 +6,7 @@ export const logApiError = (url: string, response: Response) => {
     `${url} - ${response.status} ${response.statusText}`;
 
   if (frontendlogger) {
-    frontendlogger.error(error),
+    frontendlogger.error(error);
     frontendlogger.event("personopplysninger.apikall", {
       status: response.status,
       statusText: response.statusText,
