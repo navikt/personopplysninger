4b76174dc851f0aa59b8c0ca943eac7141730857
Morten Jansrud <morten@founder.no>
2019-05-15 12:20:00 +0200

    Oppdater arbeidsforhold, legg til kilde og disclaimer

--- src/sections/5-arbeidsforhold/Arbeidsforhold.tsx
+++ src/sections/5-arbeidsforhold/Arbeidsforhold.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Box from "../../components/box/Box";
 import NavArbeidsforhold from "@navikt/arbeidsforhold";
 import arbeidsforholdIkon from "../../assets/img/arbeidsforhold.svg";
+import Kilde from "../../components/kilde/Kilde";
+import { Undertekst } from "nav-frontend-typografi";
+import {FormattedHTMLMessage} from "react-intl"
 
 const Arbeidsforhold = () => (
   <Box
@@ -10,7 +13,16 @@ const Arbeidsforhold = () => (
     beskrivelse="arbeidsforhold.beskrivelse"
     icon={arbeidsforholdIkon}
   >
+    <hr className="box__linje-bred" />
     <NavArbeidsforhold />
+    <div className="arbeidsforhold__disclaimer">
+      <Undertekst>
+        <FormattedHTMLMessage id="arbeidsforhold.disclaimer" />
+      </Undertekst>
+    </div>
+    <Kilde
+      kilde="arbeidsforhold.kilde"
+    />
   </Box>
 );
 export default Arbeidsforhold;
