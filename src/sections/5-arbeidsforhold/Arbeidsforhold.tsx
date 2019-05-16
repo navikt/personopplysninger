9f60c596d909b1aad6b40a8988e934696e283c77
Morten Jansrud <morten@founder.no>
2019-05-16 16:16:09 +0200

    Legg til støtte for miljø

--- src/sections/5-arbeidsforhold/Arbeidsforhold.tsx
+++ src/sections/5-arbeidsforhold/Arbeidsforhold.tsx
@@ -4,12 +4,16 @@ import { ListeMedArbeidsforhold } from "@navikt/arbeidsforhold";
 import arbeidsforholdIkon from "../../assets/img/arbeidsforhold.svg";
 import Kilde from "../../components/kilde/Kilde";
 import { Undertekst } from "nav-frontend-typografi";
-import {FormattedHTMLMessage} from "react-intl"
+import { FormattedHTMLMessage } from "react-intl";
+import Environment from "../../utils/Environments";
+
+const environment = Environment();
+const miljo = environment.miljo as "LOCAL" | "DEV" | "PROD";
 
 const Arbeidsforhold = () => {
   const onClick = (arbeidsforholdId: string) => {
-    console.log(arbeidsforholdId)
-  }
+    console.log(arbeidsforholdId);
+  };
 
   return (
     <Box
@@ -18,17 +22,16 @@ const Arbeidsforhold = () => {
       beskrivelse="arbeidsforhold.beskrivelse"
       icon={arbeidsforholdIkon}
     >
-      <hr className="box__linje-bred" />
-      <ListeMedArbeidsforhold onClick={onClick}  />
+      <div className="arbeidsforhold">
+        <ListeMedArbeidsforhold miljo={miljo} onClick={onClick} />
+      </div>
       <div className="arbeidsforhold__disclaimer">
         <Undertekst>
           <FormattedHTMLMessage id="arbeidsforhold.disclaimer" />
         </Undertekst>
       </div>
-      <Kilde
-        kilde="arbeidsforhold.kilde"
-      />
+      <Kilde kilde="arbeidsforhold.kilde" />
     </Box>
   );
-}
+};
 export default Arbeidsforhold;
