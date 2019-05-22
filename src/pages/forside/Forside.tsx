4a079b3f7961bdfa0e07b18f17b0e3f6fcb66d60
Morten Jansrud <morten@founder.no>
2019-05-22 16:04:12 +0200

    Fjern arbeidsforhold fra forsiden

--- src/pages/forside/Forside.tsx
+++ src/pages/forside/Forside.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Sidetittel from "./sections/1-sidetittel/Sidetittel";
 import Brodsmulesti from "./sections/2-brodsmulesti/Brodsmulesti";
 import PersonInfo from "./sections/4-personinfo/PersonInfo";
-import Arbeidsforhold from "./sections/5-arbeidsforhold/Arbeidsforhold";
 import EksterneLenker from "./sections/6-eksterne/EksterneLenker";
 import MerInformasjon from "./sections/7-informasjon/MerInformasjon";
 
@@ -11,7 +10,6 @@ const Forside = () => (
     <Brodsmulesti />
     <Sidetittel />
     <PersonInfo />
-    <Arbeidsforhold />
     <EksterneLenker />
     <MerInformasjon />
   </>
