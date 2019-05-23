1864f8a2be9a0caf777ed2b2f21b5298228e1d54
Morten Jansrud <morten@founder.no>
2019-05-23 14:11:00 +0200

    Legg til baseUrl

--- src/utils/Environments.ts
+++ src/utils/Environments.ts
@@ -1,22 +1,16 @@
 const Environment = () => {
   if (process.env.NODE_ENV === "development") {
     return {
+      baseUrl: "http://localhost:8080/person/personopplysninger",
       apiUrl: "http://localhost:8080/person/personopplysninger-api",
       tjenesteUrl: "https://tjenester-q0.nav.no",
       loginUrl: "http://localhost:8080/personbruker-api/local/cookie",
       logoutUrl: "#"
     };
   }
-  if (window.location.hostname.indexOf("www-t6") > -1) {
-    return {
-      apiUrl: `https://personopplysninger-api-t6.nais.oera-q.local/personopplysninger-api`,
-      tjenesteUrl: "https://tjenester-t6.nav.no",
-      loginUrl: "https://loginservice-q.nav.no/login",
-      logoutUrl: "https://loginservice-q.nav.no/slo"
-    };
-  }
   if (window.location.hostname.indexOf("www-q0") > -1) {
     return {
+      baseUrl: "https://www-q0.nav.no/person/personopplysninger",
       apiUrl: "https://www-q0.nav.no/person/personopplysninger-api",
       tjenesteUrl: "https://tjenester-q0.nav.no",
       loginUrl: "https://loginservice-q.nav.no/login",
@@ -25,14 +19,15 @@ const Environment = () => {
   }
   if (window.location.hostname.indexOf("www-q1") > -1) {
     return {
+      baseUrl: "https://www-q1.nav.no/person/personopplysninger",
       apiUrl: "https://www-q1.nav.no/person/personopplysninger-api",
       tjenesteUrl: "https://tjenester-q1.nav.no",
       loginUrl: "https://loginservice-q.nav.no/login",
       logoutUrl: "https://loginservice-q.nav.no/slo"
     };
   }
-
   return {
+    baseUrl: "https://www.nav.no/person/personopplysninger",
     apiUrl: "https://www.nav.no/person/personopplysninger-api",
     tjenesteUrl: "https://tjenester.nav.no",
     loginUrl: "https://loginservice.nav.no/login",
