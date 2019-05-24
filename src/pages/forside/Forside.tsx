edb657c24633c1dda4053b11a1fac00dc6d273b2
Morten Jansrud <morten@founder.no>
2019-05-24 10:51:56 +0200

    Reintroduser feature toggle for arbeidsforhold

--- src/pages/forside/Forside.tsx
+++ src/pages/forside/Forside.tsx
@@ -5,16 +5,22 @@ import PersonInfo from "./sections/4-personinfo/PersonInfo";
 import Arbeidsforhold from "./sections/5-arbeidsforhold/Arbeidsforhold";
 import EksterneLenker from "./sections/6-eksterne/EksterneLenker";
 import MerInformasjon from "./sections/7-informasjon/MerInformasjon";
+import { useStore } from "../../providers/Provider";
 
-const Forside = () => (
-  <>
-    <Brodsmulesti />
-    <Sidetittel />
-    <PersonInfo />
-    <Arbeidsforhold />
-    <EksterneLenker />
-    <MerInformasjon />
-  </>
-);
+const Forside = () => {
+  const [{ featureToggles }] = useStore();
+  return (
+    <>
+      <Brodsmulesti />
+      <Sidetittel />
+      <PersonInfo />
+      {featureToggles["personopplysninger.arbeidsforhold.liste"] && (
+        <Arbeidsforhold />
+      )}
+      <EksterneLenker />
+      <MerInformasjon />
+    </>
+  );
+};
 
 export default Forside;
