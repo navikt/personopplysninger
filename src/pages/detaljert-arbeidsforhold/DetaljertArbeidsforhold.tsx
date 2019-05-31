19bec9f08ef3cfea6e7e63c4e09c58a7f4a05742
Morten Jansrud <morten@founder.no>
2019-05-31 15:15:00 +0200

    Brodsmulesti på detaljert arbeidsforhold

--- src/pages/detaljert-arbeidsforhold/DetaljertArbeidsforhold.tsx
+++ src/pages/detaljert-arbeidsforhold/DetaljertArbeidsforhold.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { DetaljertArbeidsforhold } from "@navikt/arbeidsforhold";
 import Environment from "../../utils/Environments";
 import { withRouter, RouteComponentProps } from "react-router";
 import { VenstreChevron } from "nav-frontend-chevron";
 import { HashLink as Link } from "react-router-hash-link";
 import { baseUrl } from "../../App";
+import Brodsmulesti from "../forside/sections/2-brodsmulesti/Brodsmulesti";
 
 const environment = Environment();
 const miljo = environment.miljo as "LOCAL" | "DEV" | "PROD";
@@ -14,12 +15,19 @@ interface Routes {
 }
 
 const radix = 10;
-const Arbeidsforhold = ({ match }: RouteComponentProps<Routes>) => {
+const Arbeidsforhold = ({ match, history }: RouteComponentProps<Routes>) => {
   const id: number = parseInt(match.params.id, radix);
+
+  const goBack = (event: MouseEvent): void => {
+    event.preventDefault();
+    history.goBack();
+  };
+
   return (
     <div className="da__container">
+      <Brodsmulesti />
       <div className="da__back">
-        <Link to={`${baseUrl}/#arbeidsforhold`}>
+        <Link to={`${baseUrl}/`} onClick={goBack}>
           <VenstreChevron />
           Tilbake
         </Link>
