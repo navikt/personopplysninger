926b4a7be15bbdab934ae6abdada18ad54e176b0
Morten Jansrud <mjansrud@users.noreply.github.com>
2019-06-03 09:33:07 +0200

    Legg til ikon på detaljert visning (#70)
    * Legg til ikon på detaljert visning

--- src/pages/detaljert-arbeidsforhold/DetaljertArbeidsforhold.tsx
+++ src/pages/detaljert-arbeidsforhold/DetaljertArbeidsforhold.tsx
@@ -6,6 +6,8 @@ import { VenstreChevron } from "nav-frontend-chevron";
 import { HashLink as Link } from "react-router-hash-link";
 import { baseUrl } from "../../App";
 import Brodsmulesti from "../forside/sections/2-brodsmulesti/Brodsmulesti";
+import Icon from "../../components/icon/Icon";
+import arbeidsforholdIkon from "../../assets/img/arbeidsforhold.svg";
 import { Systemtittel } from "nav-frontend-typografi";
 import { FormattedMessage } from "react-intl";
 
@@ -28,6 +30,9 @@ const Arbeidsforhold = ({ match, history }: RouteComponentProps<Routes>) => {
   return (
     <div className="da__container">
       <Brodsmulesti />
+      <div className="da__icon">
+        <Icon backgroundImage={arbeidsforholdIkon} backgroundColor="#99C1E9" />
+      </div>
       <div className="da__rad">
         <div className="da__back">
           <Link to={`${baseUrl}/`} onClick={goBack}>
