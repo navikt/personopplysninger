b6a7bc815ffb79cb26144f6f0b1fe7e20697ab86
Morten Jansrud <morten@founder.no>
2019-06-13 08:56:01 +0200

    Forbered for naiserator

---
+++ server/server.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const mustacheExpress = require("mustache-express");
+const getDecorator = require("./dekorator");
+const { getSecrets, getMockSecrets } = require("./getSecrets");
+
+const server = express();
+
+server.set("views", `${__dirname}/../build`);
+server.set("view engine", "mustache");
+server.engine("html", mustacheExpress());
+
+// parse application/json
+server.use(express.json());
+
+server.use((req, res, next) => {
+  res.removeHeader("X-Powered-By");
+  next();
+});
+
+const renderApp = decoratorFragments =>
+  new Promise((resolve, reject) =>
+    server.render("index.html", decoratorFragments, (err, html) => {
+      if (err) reject(err);
+      resolve(html);
+    })
+  );
+
+const startServer = html => {
+  server.use("/soknader", (req, res) => {
+    res.send(html);
+  });
+
+  server.get("/soknader/internal/alive|ready", (req, res) =>
+    res.sendStatus(200)
+  );
+
+  server.use(
+    "/personopplysninger/static",
+    express.static(path.resolve(`${__dirname}/..`, "build/static"))
+  );
+
+  const port = process.env.PORT || 8080;
+  server.listen(port, () => console.log(`App listening on port: ${port}`));
+};
+
+const logError = (errorMessage, details) => console.log(errorMessage, details);
+
+if (process.env.NODE_ENV === "production") {
+  getDecorator()
+    .then(renderApp, error => logError("Failed to get decorator", error))
+    .then(startServer, error => logError("Failed to render app", error));
+}
+
+process.on("SIGTERM", () =>
+  setTimeout(() => console.log("Har sovet i 30 sekunder"), 30000)
+);
