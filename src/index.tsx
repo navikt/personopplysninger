c423df3c4d32c7e3e0e3e7a702818241429c6e74
Morten Jansrud <morten@founder.no>
2019-06-18 10:19:36 +0200

    Legg til mock for dekoratøren

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import "core-js/fn/array/includes";
 import React from "react";
 import ReactDOM from "react-dom";
 import { IntlProvider, addLocaleData } from "react-intl";
-import { setUpMock } from "./clients/apiMock";
 import { StoreProvider } from "./providers/Provider";
+import * as serviceWorker from "./service-worker";
 import { initialState, reducer } from "./providers/Store";
 import nb from "react-intl/locale-data/nb";
 import nbMessages from "./text/nb";
+
+import withMenu from "./clients/apiMock/decorator/decorator-header-withmenu";
+import megamenu from "./clients/apiMock/decorator/decorator-megamenu";
+import footer from "./clients/apiMock/decorator/decorator-footer";
+import scripts from "./clients/apiMock/decorator/decorator-scripts";
+import skiplinks from "./clients/apiMock/decorator/decorator-skiplinks";
+import styles from "./clients/apiMock/decorator/decorator-styles";
+
 import App from "./App";
 
 // Intl polyfill
@@ -20,16 +28,44 @@ const messages = {
 
 addLocaleData([...nb]);
 
-if (process.env.NODE_ENV === "development") {
-  setUpMock();
-}
+const init = async () => {
+  if (process.env.NODE_ENV === "development") {
+    await import("./clients/apiMock").then(({ setUpMock }) => setUpMock());
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{NAV_HEADING}}}",
+      withMenu
+    );
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{NAV_FOOTER}}}",
+      footer
+    );
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{NAV_STYLES}}}",
+      styles
+    );
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{NAV_SCRIPTS}}}",
+      scripts
+    );
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{NAV_SKIPLINKS}}}",
+      skiplinks
+    );
+    document.body.innerHTML = document.body.innerHTML.replace(
+      "{{{MEGAMENU_RESOURCES}}}",
+      megamenu
+    );
+  }
 
-const content = (
-  <IntlProvider locale={browserLanguage} messages={messages[browserLanguage]}>
-    <StoreProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StoreProvider>
-  </IntlProvider>
-);
+  ReactDOM.render(
+    <IntlProvider locale={browserLanguage} messages={messages[browserLanguage]}>
+      <StoreProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StoreProvider>
+    </IntlProvider>,
+    document.getElementById("app")
+  );
+  serviceWorker.unregister();
+};
+init();
 
-ReactDOM.render(content, document.getElementById("app"));
