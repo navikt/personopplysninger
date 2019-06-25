71eba176d1ab6dbdb3eb8345252ea039a9294af7
Morten Jansrud <morten@founder.no>
2019-06-25 09:22:14 +0200

    Legg til modal og i

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Ingress, Systemtittel } from "nav-frontend-typografi";
 import { FormattedMessage, injectIntl, InjectedIntlProps } from "react-intl";
-import Icon from "../icon/Icon";
+import Veilederpanel from "nav-frontend-veilederpanel";
+import Informasjon from "../../assets/img/Informasjon.svg";
+import Modal from "nav-frontend-modal";
 
 interface Props {
   id: string;
@@ -11,31 +13,55 @@ interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
+Modal.setAppElement("#app");
 const Box = (props: Props & InjectedIntlProps) => {
   const { tittel, beskrivelse, icon, children, id } = props;
+  const [visBeskrivelse, settVisBeskrivelse] = useState(false);
 
   return (
-    <div className="box__container seksjon">
-      <div className="box__icon-container icon__container" id={id}>
-        {icon && <Icon backgroundImage={icon} />}
-      </div>
-      <div className="box__content-container">
+    <div className="box__container">
+      <Veilederpanel
+        svg={<img src={icon} className="header__ikon" alt="Veileder" />}
+        type={"plakat"}
+        kompakt
+      >
         <div className="box__header" id={id}>
-          {tittel && (
-            <Systemtittel>
-              <FormattedMessage id={tittel} />
-            </Systemtittel>
-          )}
-          {beskrivelse && (
-            <div className="box__ingress">
-              <Ingress>
-                <FormattedMessage id={beskrivelse} />
-              </Ingress>
-            </div>
-          )}
+          <div className="box__title-container">
+            {beskrivelse && <div className="box__title-filler" />}
+            {tittel && (
+              <Systemtittel>
+                <FormattedMessage id={tittel} />
+              </Systemtittel>
+            )}
+            {beskrivelse && (
+              <img
+                src={Informasjon}
+                className="box__title-i-icon"
+                alt="Vis mer informasjon"
+                onClick={() => settVisBeskrivelse(!visBeskrivelse)}
+              />
+            )}
+          </div>
         </div>
         <div className="box__content">{children}</div>
-      </div>
+        {beskrivelse && (
+          <Modal
+            isOpen={visBeskrivelse}
+            onRequestClose={() => settVisBeskrivelse(false)}
+            closeButton={true}
+            contentLabel="Min modalrute"
+            className="box__modal"
+          >
+            <div style={{ padding: "2rem 2.5rem" }}>
+              <div className="box__ingress">
+                <Ingress>
+                  <FormattedMessage id={beskrivelse} />
+                </Ingress>
+              </div>
+            </div>
+          </Modal>
+        )}
+      </Veilederpanel>
     </div>
   );
 };
