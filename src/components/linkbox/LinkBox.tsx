3fe7ae839414f199447b4612ad373dcf8554ad39
Morten Jansrud <morten@founder.no>
2019-06-25 16:10:42 +0200

    Redesign av Flere opplysninger på Ditt NAV

--- src/components/linkbox/LinkBox.tsx
+++ src/components/linkbox/LinkBox.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Systemtittel, Normaltekst } from "nav-frontend-typografi";
+import { Element, Normaltekst } from "nav-frontend-typografi";
 import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
-import { LenkepanelBase } from "nav-frontend-lenkepanel";
 import Icon from "../icon/Icon";
+import Lenke from "nav-frontend-lenker";
+import { HoyreChevron } from "nav-frontend-chevron";
 
 export interface Props {
   id: string;
@@ -15,23 +16,26 @@ export interface Props {
 
 const LinkBox = (props: Props & InjectedIntlProps) => {
   return (
-    <LenkepanelBase href={props.url} className="box__wrapper">
+    <a className="linkbox__rad" href={props.url}>
       <div className="linkbox__icon-container icon__container">
         <Icon backgroundImage={props.icon} />
       </div>
       <div className="linkbox__content">
         <div className="linkbox__seksjon">
           <div className="linkbox__tittel">
-            <Systemtittel>
-              <FormattedMessage id={props.tittel} />
-            </Systemtittel>
+            <Lenke href={props.url}>
+              <Element>
+                <FormattedMessage id={props.tittel} />
+              </Element>
+            </Lenke>
           </div>
           <Normaltekst>
             <FormattedMessage id={props.beskrivelse} />
           </Normaltekst>
         </div>
       </div>
-    </LenkepanelBase>
+      <HoyreChevron />
+    </a>
   );
 };
 
--- src/pages/forside/sections/6-eksterne/EksterneLenker.tsx
+++ src/pages/forside/sections/6-eksterne/EksterneLenker.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import eksterneLenker from "./EksterneLenkerData";
 import LinkBox from "../../../../components/linkbox/LinkBox";
+import PanelBase from "nav-frontend-paneler";
+import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
 
 const LinksContainer = () => (
-  <div className="seksjon box-bottom-margin">
-    {eksterneLenker.map(link => (
-      <LinkBox
-        id={link.id}
-        key={link.id}
-        icon={link.icon}
-        tittel={link.tittel}
-        beskrivelse={link.beskrivelse}
-        lenkeTekst={link.lenkeTekst}
-        url={link.url}
-      />
-    ))}
-  </div>
+  <PanelBase border={true} className="el__panel">
+    <div className="el__content">
+      <Systemtittel>Flere opplysninger på Ditt NAV</Systemtittel>
+      <div className="el__info">
+        <Normaltekst>
+          Vi har også opplysninger om deg som ikke vises på denne siden. De
+          opplysningene kan du finne ved å gå til de ulike tjenestene under.
+        </Normaltekst>
+      </div>
+      {eksterneLenker.map(link => (
+        <LinkBox
+          id={link.id}
+          key={link.id}
+          icon={link.icon}
+          tittel={link.tittel}
+          beskrivelse={link.beskrivelse}
+          lenkeTekst={link.lenkeTekst}
+          url={link.url}
+        />
+      ))}
+    </div>
+  </PanelBase>
 );
 
 export default LinksContainer;
