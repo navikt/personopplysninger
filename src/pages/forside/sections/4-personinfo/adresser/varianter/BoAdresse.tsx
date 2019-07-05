0f0a3c4f4d57b54d11b364eb2308b11afcd93a1c
sisselfladby <sissel.fladby@nav.no>
2019-07-05 14:07:13 +0200

    Viser ikke NAV-kontor dersom bruker ikke har enhet, viser ikkebeskjed om ikke registrert adresse dersom bruker ikke har folkeregistrert bostedsadresse (#82)

--- src/pages/forside/sections/4-personinfo/adresser/varianter/BoAdresse.tsx
+++ src/pages/forside/sections/4-personinfo/adresser/varianter/BoAdresse.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from "moment";
-import { injectIntl, InjectedIntlProps } from "react-intl";
+import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
 import { Normaltekst } from "nav-frontend-typografi";
 import Lesmerpanel from "nav-frontend-lesmerpanel";
 import AdressePanel from "../../../../../../components/adresse/AdressePanel";
@@ -30,37 +30,52 @@ const BoAdresse = (props: Props & InjectedIntlProps) => {
   return (
     <AdressePanel tittel="adresse.bostedsadresse">
       <>
-        {adresse && <Normaltekst>{adresse}</Normaltekst>}
-        {adressetillegg && <Normaltekst>{adressetillegg}</Normaltekst>}
-        {(postnummer || postnummer) && (
+        {adresse || postnummer || poststed ? (
+          <>
+            {adresse && <Normaltekst>{adresse}</Normaltekst>}
+            {adressetillegg && <Normaltekst>{adressetillegg}</Normaltekst>}
+            {(postnummer || postnummer) && (
+              <Normaltekst>
+                {postnummer && postnummer} {poststed && poststed}
+              </Normaltekst>
+            )}
+            {((veiadresse && veiadresse.bolignummer) ||
+              kommune ||
+              formattedDate) && (
+              <Lesmerpanel
+                className="addresse__lesmer"
+                apneTekst={props.intl.formatMessage({
+                  id: "adresse.bostedsadresse.apneTekst"
+                })}
+                lukkTekst={props.intl.formatMessage({
+                  id: "adresse.bostedsadresse.lukkTekst"
+                })}
+              >
+                <ul className="list-column-2 address-columns">
+                  {veiadresse && veiadresse.bolignummer && (
+                    <ListElement
+                      titleId="adresse.bolignummer"
+                      content={veiadresse.bolignummer}
+                    />
+                  )}
+                  {kommune && (
+                    <ListElement titleId="adresse.kommune" content={kommune} />
+                  )}
+                  {formattedDate && (
+                    <ListElement
+                      titleId="adresse.dato"
+                      content={formattedDate}
+                    />
+                  )}
+                </ul>
+              </Lesmerpanel>
+            )}
+          </>
+        ) : (
           <Normaltekst>
-            {postnummer && postnummer} {poststed && poststed}
+            <FormattedMessage id="adresse.ikke.registrert" />
           </Normaltekst>
         )}
-        <Lesmerpanel
-          className="addresse__lesmer"
-          apneTekst={props.intl.formatMessage({
-            id: "adresse.bostedsadresse.apneTekst"
-          })}
-          lukkTekst={props.intl.formatMessage({
-            id: "adresse.bostedsadresse.lukkTekst"
-          })}
-        >
-          <ul className="list-column-2 address-columns">
-            {veiadresse && veiadresse.bolignummer && (
-              <ListElement
-                titleId="adresse.bolignummer"
-                content={veiadresse.bolignummer}
-              />
-            )}
-            {kommune && (
-              <ListElement titleId="adresse.kommune" content={kommune} />
-            )}
-            {formattedDate && (
-              <ListElement titleId="adresse.dato" content={formattedDate} />
-            )}
-          </ul>
-        </Lesmerpanel>
         <Kilde
           kilde="personalia.source.folkeregisteret"
           lenke="https://www.skatteetaten.no/person/folkeregister/"
