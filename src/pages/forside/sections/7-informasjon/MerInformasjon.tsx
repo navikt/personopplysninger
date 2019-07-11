de2c79b48a246bf8f7e3c0a9576b0ff6b0ac67f9
Sissel Fladby <sisfla@gmail.com>
2019-07-11 12:56:42 +0200

    Tastaturnavigasjon på mer informasjon

--- src/pages/forside/sections/7-informasjon/MerInformasjon.tsx
+++ src/pages/forside/sections/7-informasjon/MerInformasjon.tsx
@@ -7,19 +7,23 @@ import PanelBase from "nav-frontend-paneler";
 
 const AlternativListe = () => (
   <PanelBase border={true} className="el__panel">
-    <div className="el__content">
+    <div className="mi__content">
       <div className="mi__overskrift">
         <Systemtittel>
           <FormattedMessage id="alternativer.tittel" />
         </Systemtittel>
       </div>
       {merInformasjon.map(info => (
-        <MerInformasjonPanel
-          key={info.id}
-          tittel={info.tittel}
-          melding={info.melding}
-        />
+        <>
+          <hr className="mi__line" />
+          <MerInformasjonPanel
+            key={info.id}
+            tittel={info.tittel}
+            melding={info.melding}
+          />
+        </>
       ))}
+      <hr className="mi__line" />
     </div>
   </PanelBase>
 );
