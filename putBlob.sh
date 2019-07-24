7b693b4c43afe9ad55b40a8cd35655abaf50ad74
Eirik Årseth <eirik.emil@gmail.com>
2019-07-24 08:40:04 +0200

    script lagt til

---
+++ putBlob.sh
@@ -0,0 +1,77 @@
+putFile () {
+    FILENAME=$1
+    STORAGE_ACCOUNT=$2
+    CONTAINER=$3
+    AZURE_STORAGE_SAS=$4 
+
+    EXSTENSION="${FILENAME##*.}"
+   
+    case $EXSTENSION in
+
+        js)
+            CONTENT_TYPE="application/javascript" 
+            ;;
+
+        map)
+            CONTENT_TYPE="application/octet-stream" 
+            ;;
+
+        html)
+            CONTENT_TYPE="text/html" 
+            ;;
+        
+        css)
+            CONTENT_TYPE="text/css" 
+            ;;
+        
+        less)
+            CONTENT_TYPE="text/plain"
+            ;;
+        
+        svg)
+            CONTENT_TYPE="image/svg+xml"
+            ;;
+
+        json)
+            CONTENT_TYPE="application/json" 
+            ;;
+
+        png)
+            CONTENT_TYPE="image/png" 
+            ;;
+
+        jpeg)
+            CONTENT_TYPE="image/jpeg" 
+            ;;
+
+        jpg)
+            CONTENT_TYPE="image/jpeg" 
+            ;;
+        
+        ico)
+            CONTENT_TYPE="image/vnd.microsoft.icon" 
+            ;;
+
+        gif)
+            CONTENT_TYPE="image/gif" 
+            ;;
+
+        *)
+            exit
+            ;;
+        
+    esac
+    echo "Uploading file with name: $FILENAME"
+    echo "With content-type: $CONTENT_TYPE"
+    echo "To storage account: $STORAGE_ACCOUNT"
+    echo "To container: $CONTAINER"
+
+    curl -i -X PUT -T "$FILENAME" -H "content-type: $CONTENT_TYPE" -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://$STORAGE_ACCOUNT.blob.core.windows.net/$CONTAINER/$FILENAME$AZURE_STORAGE_SAS"
+
+}
+
+putFile $1 $2 $3 $4
+# export -f putFile
+
+
+#find ./public -type f -exec bash -c putFile {} \;
