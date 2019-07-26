0e6ff7e925c99776e1369455d937acf577954ea9
Morten Jansrud <morten@founder.no>
2019-07-26 14:36:02 +0200

    Beskytt dsop bak feature-toggles

--- src/pages/forside/sections/6-eksterne/EksterneLenker.tsx
+++ src/pages/forside/sections/6-eksterne/EksterneLenker.tsx
@@ -5,41 +5,48 @@ import PanelBase from "nav-frontend-paneler";
 import { Normaltekst, Systemtittel } from "nav-frontend-typografi";
 import dineSaker from "../../../../assets/img/DineSaker.svg";
 import { FormattedMessage } from "react-intl";
+import { useStore } from "../../../../providers/Provider";
+import WithFeatureToggles from "../../../../App";
 
-const LinksContainer = () => (
-  <PanelBase border={true} className="el__panel">
-    <div className="el__content">
-      <Systemtittel>
-        <FormattedMessage id="eksternelenker.tittel" />
-      </Systemtittel>
-      <div className="el__info">
-        <Normaltekst>
-          <FormattedMessage id="eksternelenker.beskrivelse" />
-        </Normaltekst>
+const LinksContainer = () => {
+  const [{ featureToggles }] = useStore();
+  return (
+    <PanelBase border={true} className="el__panel">
+      <div className="el__content">
+        <Systemtittel>
+          <FormattedMessage id="eksternelenker.tittel" />
+        </Systemtittel>
+        <div className="el__info">
+          <Normaltekst>
+            <FormattedMessage id="eksternelenker.beskrivelse" />
+          </Normaltekst>
+        </div>
+        {eksterneLenker.map(link => (
+          <LinkBox
+            id={link.id}
+            key={link.id}
+            icon={link.icon}
+            tittel={link.tittel}
+            beskrivelse={link.beskrivelse}
+            lenkeTekst={link.lenkeTekst}
+            to={link.url}
+            component={"a"}
+          />
+        ))}
+        {featureToggles.data["personopplysninger.dsop"] && (
+          <LinkBox
+            id={"dsop"}
+            icon={dineSaker}
+            tittel={"eksternelenker.dsop.tittel"}
+            beskrivelse={"eksternelenker.dsop.beskrivelse"}
+            lenkeTekst={"eksternelenker.dsop.lenkeTekst"}
+            to={"/person/personopplysninger/dsop/historikk"}
+            component={"Link"}
+          />
+        )}
       </div>
-      {eksterneLenker.map(link => (
-        <LinkBox
-          id={link.id}
-          key={link.id}
-          icon={link.icon}
-          tittel={link.tittel}
-          beskrivelse={link.beskrivelse}
-          lenkeTekst={link.lenkeTekst}
-          to={link.url}
-          component={"a"}
-        />
-      ))}
-      <LinkBox
-        id={"dsop"}
-        icon={dineSaker}
-        tittel={"eksternelenker.dsop.tittel"}
-        beskrivelse={"eksternelenker.dsop.beskrivelse"}
-        lenkeTekst={"eksternelenker.dsop.lenkeTekst"}
-        to={"/person/personopplysninger/dsop/historikk"}
-        component={"Link"}
-      />
-    </div>
-  </PanelBase>
-);
+    </PanelBase>
+  );
+};
 
 export default LinksContainer;
