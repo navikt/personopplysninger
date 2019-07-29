077135fea2d40b22081bd03cad0415f148e761a0
Morten Jansrud <morten@founder.no>
2019-07-29 10:39:11 +0200

    Vis felter i logg

--- src/pages/dsop/Detaljer.tsx
+++ src/pages/dsop/Detaljer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { DsopInfo } from "../../types/dsop";
 import { RouteComponentProps, withRouter } from "react-router-dom";
+import { Normaltekst, Undertittel } from "nav-frontend-typografi";
+import ListElement from "../../components/listelement/ListElement";
+import moment from "moment";
 
 interface Props {
   dsopInfo: DsopInfo;
@@ -19,7 +22,36 @@ const DsopDetaljer = (props: Props & RouteComponentProps<Routes>) => {
     .shift();
 
   console.log(dsopInnslag);
-  return <div>Test</div>;
+  return dsopInnslag ? (
+    <div>
+      <Undertittel>Mottaker</Undertittel>
+      <Normaltekst>{dsopInnslag.mottaker}</Normaltekst>
+      <hr className="box__linje-bred" />
+      <div className="box">
+        <div className="box__content">
+          <ul className="list-column-2">
+            <ListElement
+              titleId={"dsop.uthentingstidspunkt"}
+              content={moment(dsopInnslag.uthentingsTidspunkt).format(
+                "DD.MM.YYYY hh:mm"
+              )}
+            />
+            <ListElement
+              titleId={"dsop.personnummer"}
+              content={dsopInnslag.person}
+            />
+            <ListElement titleId={"dsop.tema"} content={dsopInnslag.tema} />
+            <ListElement
+              titleId={"dsop.behanglingsGrunnlag"}
+              content={dsopInnslag.behandlingsGrunnlag}
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div>Fant ikke innslag</div>
+  );
 };
 
 export default withRouter(DsopDetaljer);
