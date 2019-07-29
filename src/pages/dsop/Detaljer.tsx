b37770bba30deb25f9a1d4dd159244047c1d8551
Morten Jansrud <morten@founder.no>
2019-07-29 11:04:47 +0200

    Pretty print json

--- src/pages/dsop/Detaljer.tsx
+++ src/pages/dsop/Detaljer.tsx
@@ -3,6 +3,7 @@ import { DsopInfo } from "../../types/dsop";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Normaltekst, Undertittel } from "nav-frontend-typografi";
 import ListElement from "../../components/listelement/ListElement";
+import ReactJson from "react-json-view";
 import moment from "moment";
 
 interface Props {
@@ -21,34 +22,46 @@ const DsopDetaljer = (props: Props & RouteComponentProps<Routes>) => {
     .filter(d => d.uthentingsTidspunkt === id)
     .shift();
 
-  console.log(dsopInnslag);
   return dsopInnslag ? (
-    <div>
-      <Undertittel>Mottaker</Undertittel>
-      <Normaltekst>{dsopInnslag.mottaker}</Normaltekst>
-      <hr className="box__linje-bred" />
-      <div className="box">
-        <div className="box__content">
-          <ul className="list-column-2">
-            <ListElement
-              titleId={"dsop.uthentingstidspunkt"}
-              content={moment(dsopInnslag.uthentingsTidspunkt).format(
-                "DD.MM.YYYY hh:mm"
-              )}
-            />
-            <ListElement
-              titleId={"dsop.personnummer"}
-              content={dsopInnslag.person}
-            />
-            <ListElement titleId={"dsop.tema"} content={dsopInnslag.tema} />
-            <ListElement
-              titleId={"dsop.behanglingsGrunnlag"}
-              content={dsopInnslag.behandlingsGrunnlag}
-            />
-          </ul>
+    (() => {
+      const dsopUtleverteData = JSON.parse(atob(dsopInnslag.leverteData));
+
+      return (
+        <div>
+          <Undertittel>Mottaker</Undertittel>
+          <Normaltekst>{dsopInnslag.mottaker}</Normaltekst>
+          <hr className="box__linje-bred" />
+          <div className="box">
+            <div className="box__content">
+              <ul className="list-column-2">
+                <ListElement
+                  titleId={"dsop.uthentingstidspunkt"}
+                  content={moment(dsopInnslag.uthentingsTidspunkt).format(
+                    "DD.MM.YYYY hh:mm"
+                  )}
+                />
+                <ListElement
+                  titleId={"dsop.personnummer"}
+                  content={dsopInnslag.person}
+                />
+                <ListElement titleId={"dsop.tema"} content={dsopInnslag.tema} />
+                <ListElement
+                  titleId={"dsop.behanglingsGrunnlag"}
+                  content={dsopInnslag.behandlingsGrunnlag}
+                />
+              </ul>
+            </div>
+            <div className="detaljer__container">
+              <div className="detaljer__header">
+                <Undertittel>Leverte data</Undertittel>
+              </div>
+              <hr className="box__linje-bred" />
+              <ReactJson src={dsopUtleverteData} />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      );
+    })()
   ) : (
     <div>Fant ikke innslag</div>
   );
