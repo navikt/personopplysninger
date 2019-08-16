b180cec08f395f505c67ffbeb690013b6ca05e32
Morten Jansrud <morten.jansrud@nav.no>
2019-08-16 13:15:20 +0200

    Fjern ubrukte imports

--- src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/TelefonnummerHosNav.tsx
+++ src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/TelefonnummerHosNav.tsx
@@ -19,8 +19,6 @@ interface Props {
 
 const Telefonnummer = (props: Props) => {
   const [{ featureToggles }] = useStore();
-  const [loading, settLoading] = useState(false);
-  const [error, settError] = useState();
   const [edit, settEdit] = useState(false);
   const { tlfnr } = props;
 
@@ -37,82 +35,89 @@ const Telefonnummer = (props: Props) => {
   };
 
   const send = (e: FormContext) => {
-    const { isValid, fields } = e;
+    const { isValid } = e;
 
     if (isValid) {
       console.log("valid! yay");
-      settLoading(true);
-      settLoading(false);
     }
   };
 
   return (
     <FormValidation onSubmit={send} config={formConfig}>
-      {({ errors, fields, submitted }) => (
-        <>
-          <div className="underseksjon__header">
-            <Undertittel>
-              <FormattedMessage id="personalia.tlfnr.oveskrift" />
-            </Undertittel>
-          </div>
-          {tlfnr && (tlfnr.jobb || tlfnr.mobil || tlfnr.privat) ? (
-            <>
-              <ul className="list-column-2">
-                <ListElement
-                  titleId="personalia.tlfnr.jobb"
-                  content={tlfnr.jobb}
-                />
-                <ListElement
-                  titleId="personalia.tlfnr.mobil"
-                  content={tlfnr.mobil}
-                />
-                <ListElement
-                  titleId="personalia.tlfnr.privat"
-                  content={tlfnr.privat}
-                />
-              </ul>
-              {featureToggles.data["personopplysninger.pdl"] ? (
-                <Kilde
-                  kilde="personalia.source.nav"
-                  onClick={() => settEdit(true)}
-                  lenkeTekst="side.endre"
-                  lenkeType={"KNAPP"}
-                  ikon={endreIkon}
-                />
-              ) : (
-                <Kilde
-                  kilde="personalia.source.nav"
-                  lenke={`${tjenesteUrl}/brukerprofil/`}
-                  lenkeTekst="personalia.link.brukerprofil.endre"
-                  lenkeType={"EKSTERN"}
-                  ikon={endreIkon}
-                />
-              )}
-            </>
-          ) : (
-            <>
-              <Melding meldingId="personalia.tlfnr.ingenData" />
-              {featureToggles.data["personopplysninger.pdl"] ? (
-                <Kilde
-                  kilde="personalia.source.nav"
-                  onClick={() => settEdit(true)}
-                  lenkeTekst="side.leggtil"
-                  lenkeType={"KNAPP"}
-                  ikon={leggTilIkon}
-                />
-              ) : (
-                <Kilde
-                  kilde="personalia.source.nav"
-                  lenke={`${tjenesteUrl}/brukerprofil/`}
-                  lenkeTekst="personalia.link.brukerprofil.leggtil"
-                  lenkeType={"EKSTERN"}
-                  ikon={leggTilIkon}
-                />
-              )}
-            </>
-          )}
-        </>
-      )}
+      {({ errors, fields, submitted }) => {
+        console.log(errors);
+        console.log(fields);
+        console.log(submitted);
+        return (
+          <>
+            <div className="underseksjon__header">
+              <Undertittel>
+                <FormattedMessage id="personalia.tlfnr.oveskrift" />
+              </Undertittel>
+            </div>
+            {tlfnr && (tlfnr.jobb || tlfnr.mobil || tlfnr.privat) ? (
+              <>
+                <ul className="list-column-2">
+                  {edit ? (
+                    <div />
+                  ) : (
+                    <ListElement
+                      titleId="personalia.tlfnr.jobb"
+                      content={tlfnr.jobb}
+                    />
+                  )}
+                  <ListElement
+                    titleId="personalia.tlfnr.mobil"
+                    content={tlfnr.mobil}
+                  />
+                  <ListElement
+                    titleId="personalia.tlfnr.privat"
+                    content={tlfnr.privat}
+                  />
+                </ul>
+                {featureToggles.data["personopplysninger.pdl"] ? (
+                  <Kilde
+                    kilde="personalia.source.nav"
+                    onClick={() => settEdit(true)}
+                    lenkeTekst="side.endre"
+                    lenkeType={"KNAPP"}
+                    ikon={endreIkon}
+                  />
+                ) : (
+                  <Kilde
+                    kilde="personalia.source.nav"
+                    lenke={`${tjenesteUrl}/brukerprofil/`}
+                    lenkeTekst="personalia.link.brukerprofil.endre"
+                    lenkeType={"EKSTERN"}
+                    ikon={endreIkon}
+                  />
+                )}
+              </>
+            ) : (
+              <>
+                <Melding meldingId="personalia.tlfnr.ingenData" />
+                {featureToggles.data["personopplysninger.pdl"] ? (
+                  <Kilde
+                    kilde="personalia.source.nav"
+                    onClick={() => settEdit(true)}
+                    lenkeTekst="side.leggtil"
+                    lenkeType={"KNAPP"}
+                    ikon={leggTilIkon}
+                  />
+                ) : (
+                  <Kilde
+                    kilde="personalia.source.nav"
+                    lenke={`${tjenesteUrl}/brukerprofil/`}
+                    lenkeTekst="personalia.link.brukerprofil.leggtil"
+                    lenkeType={"EKSTERN"}
+                    ikon={leggTilIkon}
+                  />
+                )}
+              </>
+            )}
+          </>
+        );
+      }}
     </FormValidation>
   );
 };
