56750582bda82bd6ab25e247823e75e9b0e2b381
Morten Jansrud <morten.jansrud@nav.no>
2019-09-20 13:49:41 +0200

    Formatter preutfylt dato riktig

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,6 @@
+import moment from "moment";
+
 export const dateOneYearAhead = new Date(
   new Date().setFullYear(new Date().getFullYear() + 1)
 );
+export const oneYearAhead = moment(dateOneYearAhead).format("YYYY-MM-DD");
