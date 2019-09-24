c1b6117e2339f7cce26e9c209f48fd1fea5f5b16
Morten Jansrud <morten.jansrud@nav.no>
2019-09-24 12:40:13 +0200

    Test med babel-polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,9 +1,8 @@
+// Functions
+import "babel-polyfill";
+
 // Browsers
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/ie11";
-
-// Functions
-import "core-js/fn/array/includes";
-import "core-js/fn/array/entries";
 
 global.Intl = require("intl");
