fc3146ae568355702dd20ae3f483d65271f6e6d7
Morten Jansrud <morten.jansrud@nav.no>
2019-10-02 15:19:20 +0200

    Alert på utenlandsk adresse

--- src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/UtenlandskAdresse.tsx
+++ src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/UtenlandskAdresse.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Normaltekst } from "nav-frontend-typografi";
 import { UtenlandskAdresse as UtenlandskAdresseType } from "../../../../../../../types/adresser/utenlandskadresse";
 import GateAdresse from "../../komponenter/GateAdresse";
+import { AlertStripeInfo } from "nav-frontend-alertstriper";
+import { FormattedMessage } from "react-intl";
+import moment from "moment";
 
 interface Props {
   utenlandskadresse: UtenlandskAdresseType;
 }
 
 const UtenlandskAdresse = (props: Props) => {
-  const { adresse1, adresse2, adresse3, land } = props.utenlandskadresse;
+  const {
+    adresse1,
+    adresse2,
+    adresse3,
+    land,
+    datoTilOgMed
+  } = props.utenlandskadresse;
   return (
     <>
       <GateAdresse
@@ -21,6 +30,13 @@ const UtenlandskAdresse = (props: Props) => {
           <Normaltekst>{land}</Normaltekst>
         </div>
       )}
+      <div className="adresse__divider" />
+      <AlertStripeInfo>
+        <FormattedMessage
+          id="adresse.midlertidig.alert"
+          values={{ dato: moment(datoTilOgMed).format("LL") }}
+        />
+      </AlertStripeInfo>
     </>
   );
 };
