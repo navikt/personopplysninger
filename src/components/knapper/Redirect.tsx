bb0e6fac4c9b6fa0b01b2815274d25732ae66daa
Morten Jansrud <morten.jansrud@nav.no>
2019-10-04 14:36:39 +0200

    Juster design på redirect-knapp

--- src/components/knapper/Redirect.tsx
+++ src/components/knapper/Redirect.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import redirectsJson from "utils/redirects.json";
+import tilbakeIkon from "assets/img/Back.svg";
 
 const redirects: {
   [key: string]: { knapp: string; url: string };
@@ -14,6 +15,9 @@ const RedirectKnapp = (props: Props) => {
   return (
     <div className="redirect__container">
       <a href={redirect.url} className="redirect__wrapper">
+        <span className="kilde__icon">
+          <img src={tilbakeIkon} alt="Ekstern lenke" />
+        </span>
         {redirect.knapp}
       </a>
     </div>
