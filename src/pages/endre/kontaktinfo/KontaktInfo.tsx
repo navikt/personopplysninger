65afbc0d287d97df8b14d384e3a2f77b9a2a25ac
Morten Jansrud <morten.jansrud@nav.no>
2019-10-07 15:41:20 +0200

    Refaktor endreSide

--- src/pages/endre/kontaktinfo/KontaktInfo.tsx
+++ src/pages/endre/kontaktinfo/KontaktInfo.tsx
@@ -1,9 +1,4 @@
-import React, { useEffect } from "react";
-import { useStore } from "providers/Provider";
-import { fetchPersonInfo } from "clients/apiClient";
-import { PersonInfo } from "types/personInfo";
-import Error, { HTTPError } from "components/error/Error";
-import Spinner from "components/spinner/Spinner";
+import React from "react";
 import Adresser from "pages/forside/sections/4-personinfo/3-adresser/Adresser-PDL";
 import Utbetalinger from "../../forside/sections/4-personinfo/4-utbetalinger/Utbetalinger";
 import Box from "components/box/Box";
@@ -12,62 +7,39 @@ import RedirectKnapp from "components/knapper/Redirect";
 import { withRouter, RouteComponentProps } from "react-router";
 import PDLTelefonnummerHosNav from "../../forside/sections/4-personinfo/2-kontaktinfo/subsections/TelefonnummerHosNav-PDL";
 import Brodsmulesti from "../../forside/sections/2-brodsmulesti/Brodsmulesti";
+import MedPersonInfo from "../../../providers/personinfo/PersinInfo";
 
 interface Routes {
   tjeneste?: string;
 }
 
-const KontaktInformasjon = (props: RouteComponentProps<Routes>) => {
-  const [{ personInfo }, dispatch] = useStore();
-  const { params } = props.match;
-
-  useEffect(() => {
-    if (personInfo.status === "LOADING") {
-      fetchPersonInfo()
-        .then(personInfo =>
-          dispatch({
-            type: "SETT_PERSON_INFO_RESULT",
-            payload: personInfo as PersonInfo
-          })
-        )
-        .catch((error: HTTPError) =>
-          dispatch({ type: "SETT_PERSON_INFO_ERROR", payload: error })
-        );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  switch (personInfo.status) {
-    default:
-    case "LOADING":
-      return <Spinner />;
-    case "RESULT":
-      const { personalia, adresser } = personInfo.data;
-      return (
-        <div className="kontaktInfo__container">
-          <Brodsmulesti hierarki={[{ title: "side.endre" }]} />
-          {personalia && (
-            <Box
-              id="kontaktinformasjon"
-              tittel="kontaktinfo.tittel"
-              icon={kontaktIkon}
-            >
-              <PDLTelefonnummerHosNav tlfnr={personalia.tlfnr} />
-            </Box>
-          )}
-          {adresser && <Adresser adresser={adresser} />}
-          {personalia && (
-            <Utbetalinger
-              kontonr={personalia.kontonr}
-              utenlandskbank={personalia.utenlandskbank}
-            />
-          )}
-          {params.tjeneste && <RedirectKnapp tjeneste={params.tjeneste} />}
-        </div>
-      );
-    case "ERROR":
-      return <Error error={personInfo.error} />;
-  }
-};
+const KontaktInformasjon = (props: RouteComponentProps<Routes>) => (
+  <MedPersonInfo>
+    {({ personalia, adresser }) => (
+      <div className="kontaktInfo__container">
+        <Brodsmulesti hierarki={[{ title: "side.endre" }]} />
+        {personalia && (
+          <Box
+            id="kontaktinformasjon"
+            tittel="kontaktinfo.tittel"
+            icon={kontaktIkon}
+          >
+            <PDLTelefonnummerHosNav tlfnr={personalia.tlfnr} />
+          </Box>
+        )}
+        {adresser && <Adresser adresser={adresser} />}
+        {personalia && (
+          <Utbetalinger
+            kontonr={personalia.kontonr}
+            utenlandskbank={personalia.utenlandskbank}
+          />
+        )}
+        {props.match.params.tjeneste && (
+          <RedirectKnapp tjeneste={props.match.params.tjeneste} />
+        )}
+      </div>
+    )}
+  </MedPersonInfo>
+);
 
 export default withRouter(KontaktInformasjon);
---
+++ src/providers/personinfo/PersinInfo.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import Error, { HTTPError } from "../../components/error/Error";
+import { useStore } from "../Provider";
+import { fetchPersonInfo } from "../../clients/apiClient";
+import Spinner from "../../components/spinner/Spinner";
+import { PersonInfo } from "../../types/personInfo";
+
+export type FetchPersonInfo =
+  | { status: "LOADING" }
+  | { status: "RESULT"; data: PersonInfo }
+  | { status: "ERROR"; error: HTTPError };
+
+interface Props {
+  children: (data: PersonInfo) => JSX.Element;
+}
+
+const MedPersonInfo = (props: Props) => {
+  const [{ personInfo }, dispatch] = useStore();
+
+  useEffect(() => {
+    if (personInfo.status === "LOADING") {
+      fetchPersonInfo()
+        .then(personInfo =>
+          dispatch({
+            type: "SETT_PERSON_INFO_RESULT",
+            payload: personInfo as PersonInfo
+          })
+        )
+        .catch((error: HTTPError) =>
+          dispatch({ type: "SETT_PERSON_INFO_ERROR", payload: error })
+        );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  switch (personInfo.status) {
+    case "LOADING":
+      return <Spinner />;
+    case "RESULT":
+      return props.children(personInfo.data);
+    case "ERROR":
+      return <Error error={personInfo.error} />;
+  }
+};
+
+export default MedPersonInfo;
