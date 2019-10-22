7c3be8d7a78bc45bccb21b6563f0eac1528a534d
Morten Jansrud <morten.jansrud@nav.no>
2019-10-22 13:47:58 +0200

    Generisk  støtte for miljøer i redirects

--- src/utils/Redirects.ts
+++ src/utils/Redirects.ts
@@ -1,22 +1,25 @@
 const Redirects = () => {
+  var host = window.location.host;
+  var subdomain = host.split(".")[0];
+
   const redirects = {
-    // Prod urls
+    // Prod
     "soknad-om-pensjon": {
       url: `https://tjenester.nav.no/pselv/publisering/dinpensjon.jsf`,
       beskrivelse: `Du har blitt sendt fra pensjonsøknaden. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
       knapp: "Gå tilbake til pensjon"
     }
   };
-  if (window.location.hostname.indexOf("www-q1") > -1) {
-    redirects[
-      "soknad-om-pensjon"
-    ].url = `https://tjenester-q1.nav.no/pselv/publisering/dinpensjon.jsf`;
+
+  if (subdomain !== "www") {
+    // Q0, Q1 etc
+    const env = subdomain.split("-")[1];
+    redirects["soknad-om-pensjon"] = {
+      ...redirects["soknad-om-pensjon"],
+      url: `https://tjenester-${env}.nav.no/pselv/publisering/dinpensjon.jsf`
+    };
   }
-  if (window.location.hostname.indexOf("www-q0") > -1) {
-    redirects[
-      "soknad-om-pensjon"
-    ].url = `https://tjenester-q0.nav.no/pselv/publisering/dinpensjon.jsf`;
-  }
+
   return redirects;
 };
 
