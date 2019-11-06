35e9b7843585753840d7efff0c0dee9d3bbd1598
Morten Jansrud <morten.jansrud@nav.no>
2019-11-06 10:25:01 +0100

    Legg til beskrivelse av redirects

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -1,4 +1,24 @@
 export default {
+  /*
+    Andre tjenester kan sende brukeren til Personopplysninger for endring av kontaktinformasjon, kontonummer og midlertidig adresse.
+    Brukeren vil samtidig se en en knapp med tilhørende beskrivelse som ruter han/henne tilbake til den opprinnelige tjenesten.
+
+    Fremgangsmåte:
+    1) Legg til tjenesten i listen under
+    2) Send brukeren til https://www.nav.no/person/personopplysninger/endre-opplysninger/sendt-fra/[KEY]/[REDIRECT_URL]
+
+    Eksempel på url:
+    https://www.nav.no/person/personopplysninger/endre-opplysninger/sendt-fra/publisering/dinpensjon/https%3A%2F%2Ftjenester-q4.nav.no%2Fpselv%2Fpublisering%2Fdinpensjon.jsf
+
+    Hvordan funker det?
+    Beskrivelse og tekst på knappen vil bestemmes ut ifra [KEY].
+    Lenken tilbake settes ut ifra [REDIRECT_URL] (må være URL encoded).
+
+    --
+
+    Tillatte tjenester med redirect tilbake:
+
+   */
   "publisering/dinpensjon": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fpublisering%2Fdinpensjon.jsf.*`,
     beskrivelse: `Du har blitt sendt fra din pensjon. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
