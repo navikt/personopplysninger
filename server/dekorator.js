bbe717b19e860c438a6090e00bcb51dbcc0b397a
Morten Jansrud <morten.jansrud@nav.no>
2019-11-06 09:35:51 +0100

    Cache dekoratør

--- server/dekorator.js
+++ server/dekorator.js
@@ -1,41 +1,56 @@
-require("dotenv").config();
 const jsdom = require("jsdom");
 const request = require("request");
-
+const NodeCache = require("node-cache");
+const logger = require("./logger");
 const { JSDOM } = jsdom;
 
-const requestDecorator = callback =>
-  request(
-    process.env.NAIS_CLUSTER_NAME === "dev-sbs"
-      ? "https://www-q0.nav.no/person/nav-dekoratoren/"
-      : "https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true",
-    callback
-  );
+const SECONDS_PER_MINUTE = 60;
+const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
 
-const getDecorator = () =>
+// Refresh cache every hour
+const cache = new NodeCache({
+  stdTTL: SECONDS_PER_HOUR,
+  checkperiod: SECONDS_PER_MINUTE
+});
+
+const getUrl = namespace => {
+  if (namespace !== "p") {
+    // Q0, Q1, Q6 etc ..
+    return `https://appres-${namespace}.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true`;
+  } else {
+    // Produksjon
+    return `https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true`;
+  }
+};
+
+const getDecorator = namespace =>
   new Promise((resolve, reject) => {
-    const callback = (error, response, body) => {
-      if (!error && response.statusCode >= 200 && response.statusCode < 400) {
-        const { document } = new JSDOM(body).window;
-        const prop = "innerHTML";
-
-        const data = {
-          NAV_SKIPLINKS: document.getElementById("skiplinks")[prop],
-          NAV_SCRIPTS: document.getElementById("scripts")[prop],
-          NAV_STYLES: document.getElementById("styles")[prop],
-          NAV_HEADING: document.getElementById("header-withmenu")[prop],
-          NAV_FOOTER: document.getElementById("footer-withmenu")[prop],
-          MEGAMENU_RESOURCES: document.getElementById("megamenu-resources")[
-            prop
-          ]
-        };
-        resolve(data);
-      } else {
-        reject(new Error(error));
-      }
-    };
-
-    requestDecorator(callback);
+    const decorator = cache.get(namespace);
+    if (decorator) {
+      resolve(decorator);
+    } else {
+      request(getUrl(namespace), (error, response, body) => {
+        if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+          const { document } = new JSDOM(body).window;
+          const prop = "innerHTML";
+          const data = {
+            NAV_SKIPLINKS: document.getElementById("skiplinks")[prop],
+            NAV_SCRIPTS: document.getElementById("scripts")[prop],
+            NAV_STYLES: document.getElementById("styles")[prop],
+            NAV_HEADING: document.getElementById("header-withmenu")[prop],
+            NAV_FOOTER: document.getElementById("footer-withmenu")[prop],
+            MEGAMENU_RESOURCES: document.getElementById("megamenu-resources")[
+              prop
+            ]
+          };
+          cache.set(namespace, data);
+          logger.info(`${namespace}: Creating cache`);
+          resolve(data);
+        } else {
+          reject(new Error(error));
+        }
+      });
+    }
   });
 
 module.exports = getDecorator;
