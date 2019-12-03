88e3a0cd49888beb950f6b4cb9ffa342964f659a
Morten Jansrud <morten.jansrud@nav.no>
2019-12-03 11:08:58 +0100

    Korriger tekster

--- src/utils/redirects.ts
+++ src/utils/redirects.ts
@@ -19,37 +19,37 @@ export default {
    */
   "publisering/dinpensjon": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fpublisering%2Fdinpensjon.jsf.*`,
-    beskrivelse: `Du har blitt sendt fra din pensjon. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt fra din pensjon. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til din pensjon"
   },
   "skjema/alderspensjon": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fskjema%2Fdinpensjon.jsf.*`,
-    beskrivelse: `Du har blitt sendt fra alderspensjon. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt fra alderspensjon. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til alderspensjon"
   },
   "skjema/endringalderspensjon": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fskjema%2Fendringalderspensjon.jsf.*`,
-    beskrivelse: `Du har blitt sendt fra endring i alderspensjon. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt fra endring i alderspensjon. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til alderspensjon"
   },
   "skjema/uforetrygd": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fskjema%2Fuforetrygd.jsf.*`,
-    beskrivelse: `Du har blitt sendt skjemaet uføretrygd. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt skjemaet uføretrygd. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til uføretrygd"
   },
   "publisering/uforetrygd": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fpublisering%2Fuforetrygd.jsf.*`,
-    beskrivelse: `Du har blitt sendt fra uføretrygd. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt fra uføretrygd. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til uføretrygd"
   },
   "skjema/skjemaoversikt": {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Fskjema%2Fskjemaoversikt.jsf.*`,
-    beskrivelse: `Du har blitt sendt skjemaoversikt. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    beskrivelse: `Du har blitt sendt skjemaoversikt. Her kan du legeg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
     knapp: "Gå tilbake til skjemaoversikt"
   },
   minprofil: {
     allowed: `https%3A%2F%2Ftjenester?-?.?..nav.no%2Fpselv%2Ftransaksjon%2Fminprofil.jsf.*`,
-    beskrivelse: `Du har blitt sendt fra minprofil. Her kan du legg til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
-    knapp: "Gå tilbake til minprofil"
+    beskrivelse: `Du har blitt sendt fra Min Profil. Her kan du legge til eller endre <b>kontaktinformasjon, midlertidig adresse og kontonummer</b>.`,
+    knapp: "Gå tilbake til Min Profil"
   }
 };
