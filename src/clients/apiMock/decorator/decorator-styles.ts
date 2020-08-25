3f44b590f1548cecc56115495e38374f44a68a2c
Morten Jansrud <morten.jansrud@nav.no>
2020-08-25 15:09:25 +0200

    Fjern kode knyttet til gammel dekoratør

--- src/clients/apiMock/decorator/decorator-styles.ts
+++ src/clients/apiMock/decorator/decorator-styles.ts
@@ -1,9 +1,2 @@
 /*tslint:disable*/
-export default '<link rel="icon" type="image/x-icon" href="http://localhost:8100/dekoratoren/media/favicon.ico" />' +
-  '<link rel="icon" type="image/png" sizes="16x16" href="http://localhost:8100/dekoratoren/media/favicon-16x16.png">' +
-  '<link rel="icon" type="image/png" sizes="32x32" href="http://localhost:8100/dekoratoren/media/favicon-32x32.png">' +
-  '<link rel="apple-touch-icon" sizes="180x180" href="http://localhost:8100/dekoratoren/media/apple-touch-icon.png">' +
-  '<link rel="mask-icon" href="http://localhost:8100/dekoratoren/media/safari-pinned-tab.svg" color="#5bbad5">' +
-  '<meta name="msapplication-TileColor" content="#ffffff">' +
-  '<meta name="theme-color" content="#ffffff">' +
-  '<link href=http://localhost:8100/dekoratoren/css/client.css rel="stylesheet" />';
+export default '<link href=http://localhost:8100/dekoratoren/css/client.css rel="stylesheet" />';
