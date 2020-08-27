8d86d0516ab8037165f253ed4387e707af8a1614
Morten Jansrud <morten.jansrud@nav.no>
2020-08-27 15:14:53 +0200

    Legg til vis-flere knapp m/animasjon

--- src/pages/institusjonsopphold/InstHistorikk.tsx
+++ src/pages/institusjonsopphold/InstHistorikk.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Element, Normaltekst } from "nav-frontend-typografi";
 import moment from "moment";
 import { AlertStripeInfo } from "nav-frontend-alertstriper";
@@ -10,6 +11,8 @@ import PageContainer from "components/pagecontainer/PageContainer";
 import INSTIkon from "assets/img/Institusjonsopphold.svg";
 import WithInst from "./InstFetch";
 import Kilde from "../../components/kilde/Kilde";
+import { Flatknapp } from "nav-frontend-knapper";
+import PilNed from "assets/img/PilNed.svg";
 
 /*
   Hent data
@@ -29,13 +32,19 @@ const InstHistorikk = () => (
   Visning
 */
 const Tabell = (props: { instInfo: InstInfo }) => {
+  const [viewAmount, setViewAmount] = useState(20);
   const location = useLocation();
   const { instInfo } = props;
+
+  let animateDelay = 0;
+  let animateDelayKey = 0;
+  let animateDelaySum = 0;
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  // @ts-ignore
   return (
     <div className="arbeidsforhold__disclaimer">
       <AlertStripeInfo>
@@ -46,7 +55,7 @@ const Tabell = (props: { instInfo: InstInfo }) => {
       <div className={"inst__tabell"}>
         {instInfo.length > 0 ? (
           <>
-            <div className="historikk__flex-rad historikk__head">
+            <div className="historikk__flex-rad inst__head">
               <div className="historikk__flex-kolonne">
                 <Element>
                   <FormattedMessage id="inst.periode" />
@@ -58,37 +67,78 @@ const Tabell = (props: { instInfo: InstInfo }) => {
                 </Element>
               </div>
             </div>
-            {instInfo
-              .sort((a, b) =>
-                moment(a.startdato) > moment(b.startdato) ? -1 : 1
-              )
-              .map((innslag, i) => (
-                <div className="historikk__flex-rad" key={i}>
-                  <div className="historikk__flex-kolonne historikk__heading">
-                    <Moment format="DD.MM.YYYY">{innslag.startdato}</Moment>
-                    {` - `}
-                    {innslag.faktiskSluttdato && (
-                      <Moment format="DD.MM.YYYY">
-                        {innslag.faktiskSluttdato}
-                      </Moment>
-                    )}
-                  </div>
-                  <div className="historikk__flex-kolonne">
-                    <Link
-                      to={`${location.pathname}/${innslag.registreringstidspunkt}`}
-                      className="lenke"
+            <TransitionGroup>
+              {instInfo
+                .sort((a, b) =>
+                  moment(a.startdato) > moment(b.startdato) ? -1 : 1
+                )
+                .slice(0, viewAmount)
+                .map((innslag, i) => {
+                  if (animateDelayKey >= 20) {
+                    animateDelay = 0;
+                    animateDelayKey = 0;
+                    animateDelaySum = 0;
+                  }
+                  animateDelay = 50 + animateDelayKey * 15;
+                  animateDelaySum = +animateDelay;
+                  animateDelayKey++;
+
+                  return (
+                    <CSSTransition
+                      key={i}
+                      classNames={"inst__animate"}
+                      style={{
+                        transitionDelay: `${animateDelay}ms`,
+                        fontWeight:
+                          i > 20 && i >= viewAmount - 20 ? "bold" : "normal",
+                      }}
+                      timeout={100 + animateDelaySum}
                     >
-                      {innslag.institusjonsnavn}
-                    </Link>
-                  </div>
-                </div>
-              ))}
+                      <div className="historikk__flex-rad">
+                        <div className="historikk__flex-kolonne historikk__heading">
+                          <Moment format="DD.MM.YYYY">
+                            {innslag.startdato}
+                          </Moment>
+                          {` - `}
+                          {innslag.faktiskSluttdato && (
+                            <Moment format="DD.MM.YYYY">
+                              {innslag.faktiskSluttdato}
+                            </Moment>
+                          )}
+                        </div>
+                        <div className="historikk__flex-kolonne">
+                          <Link
+                            to={`${location.pathname}/${innslag.registreringstidspunkt}`}
+                            className="lenke"
+                          >
+                            {innslag.institusjonsnavn}
+                          </Link>
+                        </div>
+                      </div>
+                    </CSSTransition>
+                  );
+                })}
+            </TransitionGroup>
           </>
         ) : (
           <div className="historikk__ingen-data">
             <AlertStripeInfo>
               <FormattedMessage id="inst.ingendata" />
             </AlertStripeInfo>
+          </div>
+        )}
+        {instInfo.length > 20 && instInfo.length >= viewAmount && (
+          <div className={"inst__se-flere"}>
+            <Flatknapp onClick={() => setViewAmount(viewAmount + 20)}>
+              <span>
+                <Normaltekst>Se flere</Normaltekst>
+              </span>
+              <img
+                alt={"Se flere ikon"}
+                className={"inst__se-flere-icon"}
+                src={PilNed}
+              />
+            </Flatknapp>
           </div>
         )}
       </div>
