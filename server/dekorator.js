33f1f0b0e7333121c6eb53468e18830e124ab594
Morten Jansrud <morten.jansrud@nav.no>
2021-02-24 14:59:35 +0100

    Legg til server-side brødsmulesti og språkvelger

--- server/dekorator.js
+++ server/dekorator.js
@@ -2,10 +2,20 @@ const { fetchDecoratorHtml } = require("@navikt/nav-dekoratoren-moduler/ssr");
 const logger = require("./logger");
 const { ENV } = process.env;
 
+const baseUrl = `https://www.nav.no/person`;
+
 const params = {
   enforceLogin: true,
   level: "Level4",
   redirectToApp: true,
+  breadcrumbs: [
+    { url: `${baseUrl}/dittnav/`, title: "Ditt NAV" },
+    { url: `${baseUrl}/personopplysninger/`, title: `Personopplysninger` },
+  ],
+  availableLanguages: [
+    { url: `${baseUrl}/personopplysninger/nb/`, locale: "nb" },
+    { url: `${baseUrl}/personopplysninger/en/`, locale: "en" },
+  ],
 };
 
 const getIndexWithDecorator = async (res) =>
