77d7ae915cdd5d5d37da3fd57c556063413a875c
Morten Jansrud <morten.jansrud@nav.no>
2021-02-25 14:48:59 +0100

    Reformat

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const logger = require("./logger");
 const path = require("path");
-const buildPath = path.resolve(__dirname, "../build");
-const basePath = "/person/personopplysninger";
-const server = express();
 const ENV = process.env.NODE_ENV;
 const ENV_LOCAL = ".env";
 const ENV_NAIS = "/var/run/secrets/nais.io/vault/.env";
 require("dotenv").config({ path: ENV === "production" ? ENV_NAIS : ENV_LOCAL });
 const getHtmlWithDecorator = require("./dekorator");
+const buildPath = path.resolve(__dirname, "../build");
+const basePath = "/person/personopplysninger";
+const server = express();
 
 server.use(express.json());
 server.use(`${basePath}`, express.static(buildPath, { index: false }));
