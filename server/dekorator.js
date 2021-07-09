41707d7dd71d473b8770ab4eece45af8375e0d98
anders <anders@nomerstad.com>
2021-07-09 11:40:49 +0200

    Setter utloggingsvarsel-parameter

--- server/dekorator.js
+++ server/dekorator.js
@@ -1,17 +1,18 @@
 const { injectDecoratorServerSide } = require("@navikt/nav-dekoratoren-moduler/ssr");
 
 const getHtmlWithDecorator = (filePath) =>
-  injectDecoratorServerSide({
-    env: process.env.ENV,
-    filePath: filePath,
-    breadcrumbs: [
-      { url: `https://www.nav.no/person/dittnav/`, title: "Ditt NAV" },
-      { url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger` },
-    ],
-    availableLanguages: [
-      { url: `https://www.nav.no/person/personopplysninger/nb/`, locale: "nb" },
-      { url: `https://www.nav.no/person/personopplysninger/en/`, locale: "en" },
-    ],
-  });
+    injectDecoratorServerSide({
+        env: process.env.ENV,
+        filePath: filePath,
+        breadcrumbs: [
+            { url: `https://www.nav.no/person/dittnav/`, title: "Ditt NAV" },
+            { url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger` },
+        ],
+        availableLanguages: [
+            { url: `https://www.nav.no/person/personopplysninger/nb/`, locale: "nb" },
+            { url: `https://www.nav.no/person/personopplysninger/en/`, locale: "en" },
+        ],
+        utloggingsvarsel: process.env.REACT_APP_MILJO !== 'PROD'
+    });
 
 module.exports = getHtmlWithDecorator;
