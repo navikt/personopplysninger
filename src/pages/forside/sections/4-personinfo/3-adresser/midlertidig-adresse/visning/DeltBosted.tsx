de6664cad58a32306842b17f3498e8504ba68d50
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2021-12-09 14:37:41 +0100

    Lagt inn les-mer-felt med kommune for delt bosted og oppholdsadresse

--- src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/DeltBosted.tsx
+++ src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/DeltBosted.tsx
@@ -6,19 +6,36 @@ import Matrikkeladresse from "./norske-adresser/Matrikkeladresse";
 import Ukjentbosted from "./norske-adresser/Ukjentbosted";
 import AdresseKilde from "./AdresseKilde";
 import AdressePanel from "../../komponenter/AdressePanel";
+import "moment/locale/nb";
+import Lesmerpanel from "nav-frontend-lesmerpanel";
+import ListElement from "../../../../../../../components/listelement/ListElement";
+import { useIntl } from "react-intl";
 
 interface Props {
   deltBosted: IDeltBosted;
 }
 
 const DeltBosted = (props: Props) => {
+  const { formatMessage: msg } = useIntl();
+
   let adresse;
+  let kommune;
+  let bruksenhetsnummer;
+
   switch (props.deltBosted?.adresse?.type) {
     case "VEGADRESSE":
-      adresse = <Vegadresse {...props.deltBosted?.adresse} coAdressenavn={props.deltBosted?.coAdressenavn } />;
+      adresse = <Vegadresse {...props.deltBosted?.adresse}
+                            coAdressenavn={props.deltBosted?.coAdressenavn}
+                            bruksenhetsnummer={""} />;
+      kommune = props.deltBosted.adresse?.kommune;
+      bruksenhetsnummer = props.deltBosted.adresse?.bruksenhetsnummer;
       break;
     case "MATRIKKELADRESSE":
-      adresse = <Matrikkeladresse {...props.deltBosted?.adresse} coAdressenavn={props.deltBosted?.coAdressenavn } />;
+      adresse = <Matrikkeladresse {...props.deltBosted?.adresse}
+                                  coAdressenavn={props.deltBosted?.coAdressenavn}
+                                  bruksenhetsnummer={""} />;
+      kommune = props.deltBosted.adresse?.kommune;
+      bruksenhetsnummer = props.deltBosted.adresse?.bruksenhetsnummer;
       break;
     case "UTENLANDSK_ADRESSE":
       adresse = <UtenlanskAdresse {...props.deltBosted?.adresse} coAdressenavn={props.deltBosted?.coAdressenavn } />;
@@ -32,10 +49,29 @@ const DeltBosted = (props: Props) => {
 
   return (
       <AdressePanel tittel={"adresse.deltbosted"}>
-        <div>
+        <>
           {adresse}
+          {(bruksenhetsnummer || kommune) && (
+              <Lesmerpanel
+                  className="adresse__lesmer"
+                  apneTekst={msg({ id: "adresse.bostedsadresse.apneTekst" })}
+                  lukkTekst={msg({ id: "adresse.bostedsadresse.lukkTekst" })}
+              >
+                <ul className="list-column-2 address-columns">
+                  {bruksenhetsnummer && (
+                      <ListElement
+                          titleId="adresse.bolignummer"
+                          content={bruksenhetsnummer}
+                      />
+                  )}
+                  {kommune && (
+                      <ListElement titleId="adresse.kommune" content={kommune} />
+                  )}
+                </ul>
+              </Lesmerpanel>
+          )}
           <AdresseKilde kilde={props.deltBosted.kilde as string}/>
-        </div>
+        </>
       </AdressePanel>
   );
 };
--- src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/Oppholdsadresse.tsx
+++ src/pages/forside/sections/4-personinfo/3-adresser/midlertidig-adresse/visning/Oppholdsadresse.tsx
@@ -5,19 +5,36 @@ import UtenlanskAdresse from "./utenlanske-adresser/UtenlanskAdresse";
 import Matrikkeladresse from "./norske-adresser/Matrikkeladresse";
 import AdresseKilde from "./AdresseKilde";
 import AdressePanel from "../../komponenter/AdressePanel";
+import "moment/locale/nb";
+import Lesmerpanel from "nav-frontend-lesmerpanel";
+import ListElement from "../../../../../../../components/listelement/ListElement";
+import { useIntl } from "react-intl";
 
 interface Props {
   oppholdsadresse: IOppholdsadresse;
 }
 
 const Oppholdsadresse = (props: Props) => {
+  const { formatMessage: msg } = useIntl();
+
   let adresse;
+  let kommune;
+  let bruksenhetsnummer;
+
   switch (props.oppholdsadresse?.adresse?.type) {
     case "VEGADRESSE":
-      adresse = <Vegadresse {...props.oppholdsadresse?.adresse} coAdressenavn={props.oppholdsadresse?.coAdressenavn } />;
+      adresse = <Vegadresse {...props.oppholdsadresse?.adresse}
+                            coAdressenavn={props.oppholdsadresse?.coAdressenavn }
+                            bruksenhetsnummer={""}/>;
+      kommune = props.oppholdsadresse.adresse?.kommune;
+      bruksenhetsnummer = props.oppholdsadresse.adresse?.bruksenhetsnummer;
       break;
     case "MATRIKKELADRESSE":
-      adresse = <Matrikkeladresse {...props.oppholdsadresse?.adresse} coAdressenavn={props.oppholdsadresse?.coAdressenavn } />;
+      adresse = <Matrikkeladresse {...props.oppholdsadresse?.adresse}
+                                  coAdressenavn={props.oppholdsadresse?.coAdressenavn }
+                                  bruksenhetsnummer={""}/>;
+      kommune = props.oppholdsadresse.adresse?.kommune;
+      bruksenhetsnummer = props.oppholdsadresse.adresse?.bruksenhetsnummer;
       break;
     case "UTENLANDSK_ADRESSE":
       adresse = <UtenlanskAdresse {...props.oppholdsadresse?.adresse} coAdressenavn={props.oppholdsadresse?.coAdressenavn } />;
@@ -28,10 +45,29 @@ const Oppholdsadresse = (props: Props) => {
 
   return (
       <AdressePanel tittel={"adresse.oppholdsadresse"}>
-        <div>
+        <>
           {adresse}
+          {(bruksenhetsnummer || kommune) && (
+              <Lesmerpanel
+                  className="adresse__lesmer"
+                  apneTekst={msg({ id: "adresse.bostedsadresse.apneTekst" })}
+                  lukkTekst={msg({ id: "adresse.bostedsadresse.lukkTekst" })}
+              >
+                <ul className="list-column-2 address-columns">
+                  {bruksenhetsnummer && (
+                      <ListElement
+                          titleId="adresse.bolignummer"
+                          content={bruksenhetsnummer}
+                      />
+                  )}
+                  {kommune && (
+                      <ListElement titleId="adresse.kommune" content={kommune} />
+                  )}
+                </ul>
+              </Lesmerpanel>
+          )}
           <AdresseKilde kilde={props.oppholdsadresse.kilde as string}/>
-        </div>
+        </>
       </AdressePanel>
   );
 };
