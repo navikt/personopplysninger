175738d768d4becaf38ee985b95a3860db1cfcf7
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-01-03 12:22:58 +0100

    Fikset link til å endre adresse i folkeregisteret

--- src/pages/forside/sections/4-personinfo/3-adresser/kontaktadresse/visning/Folkeregisteret.tsx
+++ src/pages/forside/sections/4-personinfo/3-adresser/kontaktadresse/visning/Folkeregisteret.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Kilde from "components/kilde/Kilde";
 import { Undertittel } from "nav-frontend-typografi";
 import { FormattedMessage } from "react-intl";
+import eksternLenkeIkon from "../../../../../../../assets/img/Link.svg";
 import { Bostedsadresse as IBostedsadresse } from "../../../../../../../types/adresser/bostedsadresse";
 import { DeltBosted as IDeltBosted } from "../../../../../../../types/adresser/deltbosted";
 import { Oppholdsadresse as IOppholdsadresse } from "../../../../../../../types/adresser/oppholdsadresse";
 import Bostedsadresse from "./Bostedsadresse";
 import DeltBosted from "./DeltBosted";
 import Oppholdsadresse from "./Oppholdsadresse";
+import { useStore } from "../../../../../../../store/Context";
 
 interface Props {
   bostedsadresse?: IBostedsadresse;
@@ -16,8 +18,11 @@ interface Props {
 }
 
 const Folkeregisteret = (props: Props) => {
+  const [{ locale }] = useStore();
   const { bostedsadresse, deltBosted, oppholdsadresse } = props;
+
   if (!(bostedsadresse || deltBosted || oppholdsadresse)) { return null; }
+
   return (
     <div>
       <div className="underseksjon__header">
@@ -31,7 +36,17 @@ const Folkeregisteret = (props: Props) => {
       {oppholdsadresse && <Oppholdsadresse oppholdsadresse={oppholdsadresse}/>}
 
       {/* Kilde vil alltid være FREG i prod, kan være PDL i dev */}
-      <Kilde kilde={"personalia.source.folkeregisteret"} lenkeType={"INGEN"}/>
+      <Kilde
+          kilde="personalia.source.folkeregisteret"
+          lenke={
+              locale === "en"
+                  ? "https://www.skatteetaten.no/en/person/national-registry/"
+                  : "https://www.skatteetaten.no/person/folkeregister/"
+          }
+          lenkeTekst="personalia.link.folkeregisteret"
+          lenkeType={"EKSTERN"}
+          ikon={eksternLenkeIkon}
+      />
     </div>
   );
 };
