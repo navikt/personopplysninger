326ab3a8dc0a37587a4fa144445f7c94c861f0c2
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-03-10 10:58:13 +0100

    Logger ENV-variabel

--- server/dekorator.js
+++ server/dekorator.js
@@ -1,21 +1,24 @@
 const { injectDecoratorServerSide } = require("@navikt/nav-dekoratoren-moduler/ssr");
 
-const getHtmlWithDecorator = (filePath) =>
-  injectDecoratorServerSide({
-      env: process.env.ENV,
-      filePath: filePath,
-      enforceLogin: true,
-      level: "Level4",
-      redirectToApp: true,
-      utloggingsvarsel: true,
-      breadcrumbs: [
-          { url: `https://www.nav.no/person/dittnav/`, title: "Ditt NAV" },
-          { url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger` },
-      ],
-      availableLanguages: [
-          { url: `https://www.nav.no/person/personopplysninger/nb/`, locale: "nb" },
-          { url: `https://www.nav.no/person/personopplysninger/en/`, locale: "en" },
-      ],
-  });
+const getHtmlWithDecorator = (filePath) => {
+        console.log(process.env.ENV)
+        injectDecoratorServerSide({
+            env: process.env.ENV,
+            filePath: filePath,
+            enforceLogin: true,
+            level: "Level4",
+            redirectToApp: true,
+            utloggingsvarsel: true,
+            breadcrumbs: [
+                {url: `https://www.nav.no/person/dittnav/`, title: "Ditt NAV"},
+                {url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger`},
+            ],
+            availableLanguages: [
+                {url: `https://www.nav.no/person/personopplysninger/nb/`, locale: "nb"},
+                {url: `https://www.nav.no/person/personopplysninger/en/`, locale: "en"},
+            ],
+        })
+    }
+;
 
 module.exports = getHtmlWithDecorator;
