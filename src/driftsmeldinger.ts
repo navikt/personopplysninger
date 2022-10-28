53ef7b52e74f245a078fcdb673bb6594c66ed8e3
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-10-28 12:03:27 +0200

    Revert "Lagt inn alertstripe for treghet mot pdl-mottak"
    This reverts commit 29182fbb0cb9a249f911934e474475796e54217b.

--- src/driftsmeldinger.ts
+++ src/driftsmeldinger.ts
@@ -1,3 +1,3 @@
 export default {
-  pdl: "Endring og sletting av opplysninger kan gå litt tregere enn vanlig. Endringene går igjennom, men det kan ta litt tid før de blir synlige på denne siden.",
+  pdl: "",
 };
