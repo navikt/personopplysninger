c17e93e941c8c678a3d8ccf33a82cc232f44bb70
Terje Karlsen <terjeofnorway@gmail.com>
2023-01-12 12:20:09 +0100

    Legger til deklarasjon for å kunne bumpe til siste DS-pakker

---
+++ src/declarations.d.ts
@@ -0,0 +1,11 @@
+import { ReactEventHandler } from "react";
+
+declare global {
+  namespace React {
+    interface DOMAttributes<T> {
+      onResize?: ReactEventHandler<T> | undefined;
+      onResizeCapture?: ReactEventHandler<T> | undefined;
+      nonce?: string | undefined;
+    }
+  }
+}
