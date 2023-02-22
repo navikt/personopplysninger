7906a1c1610cc5b5a7a6ebb8641ebd0515ab73e4
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-02-22 13:43:58 +0100

    Fjernet unødvendig dayjs-locale

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ import MedlHistorikk from "./pages/medlemskap-i-folketrygden/MedlHistorikk";
 import { WithAuth } from "./store/providers/WithAuth";
 // Import this early, to ensure our own CSS gets higher specificity
 import "@navikt/ds-css";
-import dayjs from "dayjs";
 
 export const basePath = "/person/personopplysninger";
 
@@ -46,7 +45,6 @@ const App = () => {
 
   useEffect(() => {
     document.documentElement.lang = locale;
-    dayjs.locale(locale);
   }, [locale]);
 
   const basePathWithLanguage = `${basePath}/${locale}`;
