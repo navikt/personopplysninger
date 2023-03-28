c2283478c0d88dd8e75c20c0f63c123861e0609f
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-03-28 12:48:13 +0200

    Revert "Revert "Rekkefolge kontaktinformasjon""
    This reverts commit 1f0fb374ca515430046c5bc22f16c1eed388261c.

--- src/pages/forside/sections/4-personinfo/2-kontaktinfo/KontaktInfo.tsx
+++ src/pages/forside/sections/4-personinfo/2-kontaktinfo/KontaktInfo.tsx
@@ -4,7 +4,8 @@ import kontaktIkon from "assets/img/Kontakt.svg";
 import TelefonnummerHosNav from "./subsections/telefonnummer/TelefonnummerHosNav";
 import DKIF from "./subsections/kontakt-og-reservasjonsregisteret/DKIF-Fetch";
 import { Tlfnr } from "types/personalia";
-
+import { Alert } from "@navikt/ds-react";
+import { FormattedMessage } from "react-intl";
 interface Props {
   tlfnr?: Tlfnr;
 }
@@ -14,11 +15,38 @@ const KontaktInfo = (props: Props) => {
     <Box
       id="kontaktinformasjon"
       tittel="kontaktinfo.tittel"
+      beskrivelse="kontaktinformasjon-kilde"
       icon={kontaktIkon}
       visAnkerlenke={true}
     >
-      <TelefonnummerHosNav tlfnr={props.tlfnr} />
-      <DKIF />
+      {props.tlfnr ? (
+        <>
+          <TelefonnummerHosNav tlfnr={props.tlfnr} />
+          <br />
+          <DKIF />
+        </>
+      ) : (
+        <>
+          <DKIF />
+          <br />
+          <TelefonnummerHosNav tlfnr={props.tlfnr} />
+        </>
+      )}
+      <div className="arbeidsforhold__disclaimer">
+        <Alert variant="info">
+          <FormattedMessage
+            id="kontaktogreservasjonsregister-disclaimer"
+            values={{
+              br: (text) => (
+                <>
+                  <br />
+                  {text}
+                </>
+              ),
+            }}
+          />
+        </Alert>
+      </div>
     </Box>
   );
 };
