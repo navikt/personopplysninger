8fc12dbdeffa0483f15b0cfcffd511c98aff2acc
Terje Karlsen <terjeofnorway@gmail.com>
2023-05-26 11:46:09 +0200

    Fikser linting etter merge fra master

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,186 +1,155 @@
-import { logApiError } from "../utils/logger";
-import { FeatureToggles } from "../store/Store";
-import { OutboundTlfnummer } from "../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/telefonnummer/Telefonnummer";
-import { TPSResponse } from "../types/tps-response";
-import { Feilmelding } from "../components/httpFeilmelding/HttpFeilmelding";
-import { getLoginRedirectUrl } from "../utils/redirects";
-import {
-  OutboundNorskKontonummer,
-  OutboundUtenlandsbankonto,
-} from "../pages/forside/sections/4-personinfo/4-utbetalinger/endring/types";
+import { logApiError } from '../utils/logger';
+import { FeatureToggles } from '../store/Store';
+import { OutboundTlfnummer } from '../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/telefonnummer/Telefonnummer';
+import { TPSResponse } from '../types/tps-response';
+import { Feilmelding } from '../components/httpFeilmelding/HttpFeilmelding';
+import { getLoginRedirectUrl } from '../utils/redirects';
+import { OutboundNorskKontonummer, OutboundUtenlandsbankonto } from '../pages/forside/sections/4-personinfo/4-utbetalinger/endring/types';
 
 const parseJson = (data: Response) => data.json();
 
-const {
-  REACT_APP_API_URL,
-  REACT_APP_LOGIN_URL,
-  REACT_APP_DSOP_URL,
-  REACT_APP_INNLOGGINGSSTATUS_URL,
-} = process.env;
+const { REACT_APP_API_URL, REACT_APP_LOGIN_URL, REACT_APP_DSOP_URL, REACT_APP_INNLOGGINGSSTATUS_URL } = process.env;
 
 /*
    GET
  */
 
 const sjekkAuthHentJson = (url: string) =>
-  fetch(url, {
-    method: "GET",
-    headers: { "Content-Type": "application/json;charset=UTF-8" },
-    credentials: "include",
-  })
-    .then(sjekkAuth)
-    .then(sjekkHttpFeil)
-    .then(parseJson)
-    .catch((err: string & Feilmelding) => {
-      const error = {
-        code: err.code || 404,
-        type: err.type || "feil",
-        text: err.text ?? err,
-      };
-      logApiError(url, error);
-      throw error;
-    });
+    fetch(url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        credentials: 'include',
+    })
+        .then(sjekkAuth)
+        .then(sjekkHttpFeil)
+        .then(parseJson)
+        .catch((err: string & Feilmelding) => {
+            const error = {
+                code: err.code || 404,
+                type: err.type || 'feil',
+                text: err.text ?? err,
+            };
+            logApiError(url, error);
+            throw error;
+        });
 
-export const fetchInnloggingsStatus = () =>
-  sjekkAuthHentJson(REACT_APP_INNLOGGINGSSTATUS_URL || "");
+export const fetchInnloggingsStatus = () => sjekkAuthHentJson(REACT_APP_INNLOGGINGSSTATUS_URL || '');
 
 export const fetchFeatureToggles = (featureToggles: FeatureToggles) =>
-  sjekkAuthHentJson(
-    `${REACT_APP_API_URL}/feature-toggles${getFeatureToggleUrl(featureToggles)}`
-  );
+    sjekkAuthHentJson(`${REACT_APP_API_URL}/feature-toggles${getFeatureToggleUrl(featureToggles)}`);
 
-export const fetchKontaktInfo = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/kontaktinformasjon`);
+export const fetchKontaktInfo = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/kontaktinformasjon`);
 
-export const fetchRetningsnumre = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/retningsnumre`);
+export const fetchRetningsnumre = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/retningsnumre`);
 
-export const fetchInstInfo = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/institusjonsopphold`);
+export const fetchInstInfo = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/institusjonsopphold`);
 
-export const fetchMedlInfo = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/medl`);
+export const fetchMedlInfo = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/medl`);
 
-export const fetchPostnummer = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/postnummer`);
+export const fetchPostnummer = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/postnummer`);
 
-export const fetchPersonInfo = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/personalia`);
+export const fetchPersonInfo = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/personalia`);
 
 export const fetchLand = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/land`);
 
-export const fetchValutaer = () =>
-  sjekkAuthHentJson(`${REACT_APP_API_URL}/valuta`);
+export const fetchValutaer = () => sjekkAuthHentJson(`${REACT_APP_API_URL}/valuta`);
 
-export const fetchDsopInfo = () =>
-  sjekkAuthHentJson(`${REACT_APP_DSOP_URL}/get`);
+export const fetchDsopInfo = () => sjekkAuthHentJson(`${REACT_APP_DSOP_URL}/get`);
 
 /*
     POST
  */
 
-type Outbound =
-  | OutboundTlfnummer
-  | OutboundNorskKontonummer
-  | OutboundUtenlandsbankonto;
+type Outbound = OutboundTlfnummer | OutboundNorskKontonummer | OutboundUtenlandsbankonto;
 
 const postJson = (url: string, data?: Outbound) => {
-  console.log(url, data);
-  return fetch(url, {
-    method: "POST",
-    ...(data && {
-      body: JSON.stringify(data),
-    }),
-    headers: { "Content-Type": "application/json;charset=UTF-8" },
-    credentials: "include",
-  })
-    .then(sjekkHttpFeil)
-    .then(parseJson)
-    .then(sjekkTPSFeil)
-    .catch((err: string & Feilmelding) => {
-      const error = {
-        code: err.code || 404,
-        type: err.type || "feil",
-        text: err.text ?? err,
-      };
-      logApiError(url, error);
-      throw error;
-    });
+    console.log(url, data);
+    return fetch(url, {
+        method: 'POST',
+        ...(data && {
+            body: JSON.stringify(data),
+        }),
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        credentials: 'include',
+    })
+        .then(sjekkHttpFeil)
+        .then(parseJson)
+        .then(sjekkTPSFeil)
+        .catch((err: string & Feilmelding) => {
+            const error = {
+                code: err.code || 404,
+                type: err.type || 'feil',
+                text: err.text ?? err,
+            };
+            logApiError(url, error);
+            throw error;
+        });
 };
 
-export const postTlfnummer = (data: OutboundTlfnummer) =>
-  postJson(`${REACT_APP_API_URL}/endreTelefonnummer`, data);
+export const postTlfnummer = (data: OutboundTlfnummer) => postJson(`${REACT_APP_API_URL}/endreTelefonnummer`, data);
 
-export const slettTlfnummer = (data: OutboundTlfnummer) =>
-  postJson(`${REACT_APP_API_URL}/slettTelefonnummer`, data);
+export const slettTlfnummer = (data: OutboundTlfnummer) => postJson(`${REACT_APP_API_URL}/slettTelefonnummer`, data);
 
-export const postKontonummer = (
-  data: OutboundNorskKontonummer | OutboundUtenlandsbankonto
-) => postJson(`${REACT_APP_API_URL}/endreKontonummer`, data);
+export const postKontonummer = (data: OutboundNorskKontonummer | OutboundUtenlandsbankonto) =>
+    postJson(`${REACT_APP_API_URL}/endreKontonummer`, data);
 
-export const slettKontaktadresse = () =>
-  postJson(`${REACT_APP_API_URL}/slettKontaktadresse`);
+export const slettKontaktadresse = () => postJson(`${REACT_APP_API_URL}/slettKontaktadresse`);
 
 /*
     UTILS
  */
 
-const sjekkAuth = (response: Response): any => {
-  if (response.status === 401 || response.status === 403) {
-    sendTilLogin();
-  }
-  return response;
+const sjekkAuth = (response: Response): Response => {
+    if (response.status === 401 || response.status === 403) {
+        sendTilLogin();
+    }
+    return response;
 };
 
 export const sendTilLogin = () => {
-  const redirectUrl = getLoginRedirectUrl();
-  window.location.assign(
-    `${REACT_APP_LOGIN_URL}?redirect=${redirectUrl}&level=Level4`
-  );
+    const redirectUrl = getLoginRedirectUrl();
+    window.location.assign(`${REACT_APP_LOGIN_URL}?redirect=${redirectUrl}&level=Level4`);
 };
 
-const sjekkHttpFeil = async (response: Response, showResponse = false) => {
-  if (response.ok) {
-    return response;
-  } else {
-    throw {
-      code: response.status,
-      text:
-        response.status === 400
-          ? await response.text()
-          : "Oisann, noe gikk galt! Prøv igjen senere.",
-    };
-  }
+const sjekkHttpFeil = async (response: Response) => {
+    if (response.ok) {
+        return response;
+    } else {
+        throw {
+            code: response.status,
+            text: response.status === 400 ? await response.text() : 'Oisann, noe gikk galt! Prøv igjen senere.',
+        };
+    }
 };
 
 const sjekkTPSFeil = (response: TPSResponse) => {
-  if (response.statusType === "OK") {
-    return response;
-  } else {
-    throw {
-      PENDING: {
-        type: `info`,
-        text: `Vi har sendt inn endringen din`,
-      },
-      REJECTED: {
-        type: `feil`,
-        text: `personalia.tlfnr.paagaaendeendring.feilmelding`,
-      },
-      ERROR: {
-        type: `feil`,
-        text: `${response.error && response.error.message}${
-          response.error && response.error.details
-            ? `\n${Object.values(response.error.details)
-                .map((details) => details.join("\n"))
-                .join("\n")}`
-            : ``
-        }`,
-      },
-    }[response.statusType];
-  }
+    if (response.statusType === 'OK') {
+        return response;
+    } else {
+        throw {
+            PENDING: {
+                type: 'info',
+                text: 'Vi har sendt inn endringen din',
+            },
+            REJECTED: {
+                type: 'feil',
+                text: 'personalia.tlfnr.paagaaendeendring.feilmelding',
+            },
+            ERROR: {
+                type: 'feil',
+                text: `${response.error && response.error.message}${
+                    response.error && response.error.details
+                        ? `\n${Object.values(response.error.details)
+                              .map((details) => details.join('\n'))
+                              .join('\n')}`
+                        : ''
+                }`,
+            },
+        }[response.statusType];
+    }
 };
 
 export const getFeatureToggleUrl = (featureToggles: FeatureToggles) =>
-  Object.keys(featureToggles)
-    .map((feature: string, i: number) => `${!i ? `?` : ``}feature=${feature}`)
-    .join("&");
+    Object.keys(featureToggles)
+        .map((feature: string, i: number) => `${!i ? '?' : ''}feature=${feature}`)
+        .join('&');
--- src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF-Fetch.tsx
+++ src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF-Fetch.tsx
@@ -1,48 +1,43 @@
-import React, { useEffect } from "react";
-import Error, { HTTPError } from "components/error/Error";
-import Spinner from "components/spinner/Spinner";
-import { fetchKontaktInfo } from "clients/apiClient";
-import { KontaktInfo } from "types/kontaktInfo";
-import KontaktInformasjon from "./DKIF";
-import { useStore } from "store/Context";
+import { useEffect } from 'react';
+import Error, { HTTPError } from 'components/error/Error';
+import Spinner from 'components/spinner/Spinner';
+import { fetchKontaktInfo } from 'clients/apiClient';
+import { KontaktInfo } from 'types/kontaktInfo';
+import KontaktInformasjon from './DKIF';
+import { useStore } from 'store/Context';
 
-export type FetchKontaktInfo =
-  | { status: "LOADING" }
-  | { status: "RESULT"; data: KontaktInfo }
-  | { status: "ERROR"; error: HTTPError };
+export type FetchKontaktInfo = { status: 'LOADING' } | { status: 'RESULT'; data: KontaktInfo } | { status: 'ERROR'; error: HTTPError };
 
 const DKIF = () => {
-  const [{ kontaktInfo }, dispatch] = useStore();
+    const [{ kontaktInfo }, dispatch] = useStore();
 
-  useEffect(() => {
-    if (kontaktInfo.status === "LOADING") {
-      fetchKontaktInfo()
-        .then((kontaktInfo) =>
-          dispatch({
-            type: "SETT_KONTAKT_INFO_RESULT",
-            payload: kontaktInfo as KontaktInfo,
-          })
-        )
-        .catch((error: HTTPError) =>
-          dispatch({ type: "SETT_KONTAKT_INFO_ERROR", payload: error })
-        );
-    }
-  }, [kontaktInfo, dispatch]);
-
-  return (
-    <>
-      {(() => {
-        switch (kontaktInfo.status) {
-          case "LOADING":
-            return <Spinner />;
-          case "RESULT":
-            return <KontaktInformasjon info={kontaktInfo.data} />;
-          case "ERROR":
-            return <Error error={kontaktInfo.error} />;
+    useEffect(() => {
+        if (kontaktInfo.status === 'LOADING') {
+            fetchKontaktInfo()
+                .then((kontaktInfo) =>
+                    dispatch({
+                        type: 'SETT_KONTAKT_INFO_RESULT',
+                        payload: kontaktInfo as KontaktInfo,
+                    })
+                )
+                .catch((error: HTTPError) => dispatch({ type: 'SETT_KONTAKT_INFO_ERROR', payload: error }));
         }
-      })()}
-    </>
-  );
+    }, [kontaktInfo, dispatch]);
+
+    return (
+        <>
+            {(() => {
+                switch (kontaktInfo.status) {
+                    case 'LOADING':
+                        return <Spinner />;
+                    case 'RESULT':
+                        return <KontaktInformasjon info={kontaktInfo.data} />;
+                    case 'ERROR':
+                        return <Error error={kontaktInfo.error} />;
+                }
+            })()}
+        </>
+    );
 };
 
 export default DKIF;
