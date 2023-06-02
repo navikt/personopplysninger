239ed66325fe6b29b8525bac8ef71ef7f2c9ceac
Terje Karlsen <terjeofnorway@gmail.com>
2023-06-02 10:16:07 +0200

    Fikser ny linting etter mergekonflikt

--- src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF.tsx
+++ src/pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF.tsx
@@ -1,93 +1,87 @@
-import React from "react";
-import { FormattedMessage } from "react-intl";
-import ListElement from "components/listelement/ListElement";
-import { KontaktInfo } from "types/kontaktInfo";
-import Kilde from "components/kilde/Kilde";
-import eksternLenkeIkon from "assets/img/Link.svg";
-import { useStore } from "../../../../../../../store/Context";
-import { Alert } from "@navikt/ds-react";
+import { FormattedMessage } from 'react-intl';
+import ListElement from 'components/listelement/ListElement';
+import { KontaktInfo } from 'types/kontaktInfo';
+import Kilde from 'components/kilde/Kilde';
+import eksternLenkeIkon from 'assets/img/Link.svg';
+import { useStore } from '../../../../../../../store/Context';
+import { Alert } from '@navikt/ds-react';
 
 interface Props {
-  info: KontaktInfo;
+    info: KontaktInfo;
 }
 
 const KontaktInformasjon = (props: Props) => {
-  const { mobiltelefonnummer, epostadresse, kanVarsles, spraak } = props.info;
-  const [{ locale }] = useStore();
-  return mobiltelefonnummer || epostadresse || kanVarsles ? (
-    <>
-      {" "}
-      <div className="telefonnummer">
-        <dl className="list">
-          <ListElement
-            titleId="personalia.tlfnr"
-            content={mobiltelefonnummer}
-          />
-          <ListElement titleId="personalia.spraak" content={spraak} />
-          <ListElement titleId="personalia.email" content={epostadresse} />
-        </dl>
-      </div>
-      <div className="telefonnummer">
-        <Alert variant="info" inline={true}>
-          <FormattedMessage
-            id="kontaktogreservasjonsregister-disclaimer"
-            values={{
-              br: (text) => (
-                <>
-                  <br />
-                  {text}
-                </>
-              ),
-            }}
-          />
-        </Alert>
-      </div>
-      <div className="margin-kilde">
-        <Kilde
-          kilde="personalia.source.dkif"
-          lenke={`https://brukerprofil.difi.no/minprofil${
-            locale === "en" ? "?locale=en" : ""
-          }`}
-          lenkeTekst="personalia.link.dkif.endre"
-          lenkeType={"EKSTERN"}
-          ikon={eksternLenkeIkon}
-        />
-      </div>
-    </>
-  ) : (
-    <>
-      <div className="underseksjon__beskrivelse">
-        <FormattedMessage
-          id={"personalia.dkif.ingenData"}
-          values={{
-            b: (text) => <b>{text}</b>,
-          }}
-        />
-      </div>
-      <div className="arbeidsforhold__disclaimer">
-        <Alert variant="info" inline={true}>
-          <FormattedMessage
-            id="kontaktogreservasjonsregister-disclaimer"
-            values={{
-              br: (text) => (
-                <>
-                  <br />
-                  {text}
-                </>
-              ),
-            }}
-          />
-        </Alert>
-      </div>
-      <Kilde
-        kilde="personalia.source.dkif"
-        lenke="https://brukerprofil.difi.no/minprofil"
-        lenkeTekst="personalia.link.dkif.leggtil"
-        lenkeType={"EKSTERN"}
-        ikon={eksternLenkeIkon}
-      />
-    </>
-  );
+    const { mobiltelefonnummer, epostadresse, kanVarsles, spraak } = props.info;
+    const [{ locale }] = useStore();
+    return mobiltelefonnummer || epostadresse || kanVarsles ? (
+        <>
+            {' '}
+            <div className="telefonnummer">
+                <dl className="list">
+                    <ListElement titleId="personalia.tlfnr" content={mobiltelefonnummer} />
+                    <ListElement titleId="personalia.spraak" content={spraak} />
+                    <ListElement titleId="personalia.email" content={epostadresse} />
+                </dl>
+            </div>
+            <div className="telefonnummer">
+                <Alert variant="info" inline={true}>
+                    <FormattedMessage
+                        id="kontaktogreservasjonsregister-disclaimer"
+                        values={{
+                            br: (text) => (
+                                <>
+                                    <br />
+                                    {text}
+                                </>
+                            ),
+                        }}
+                    />
+                </Alert>
+            </div>
+            <div className="margin-kilde">
+                <Kilde
+                    kilde="personalia.source.dkif"
+                    lenke={`https://brukerprofil.difi.no/minprofil${locale === 'en' ? '?locale=en' : ''}`}
+                    lenkeTekst="personalia.link.dkif.endre"
+                    lenkeType={'EKSTERN'}
+                    ikon={eksternLenkeIkon}
+                />
+            </div>
+        </>
+    ) : (
+        <>
+            <div className="underseksjon__beskrivelse">
+                <FormattedMessage
+                    id={'personalia.dkif.ingenData'}
+                    values={{
+                        b: (text) => <b>{text}</b>,
+                    }}
+                />
+            </div>
+            <div className="arbeidsforhold__disclaimer">
+                <Alert variant="info" inline={true}>
+                    <FormattedMessage
+                        id="kontaktogreservasjonsregister-disclaimer"
+                        values={{
+                            br: (text) => (
+                                <>
+                                    <br />
+                                    {text}
+                                </>
+                            ),
+                        }}
+                    />
+                </Alert>
+            </div>
+            <Kilde
+                kilde="personalia.source.dkif"
+                lenke="https://brukerprofil.difi.no/minprofil"
+                lenkeTekst="personalia.link.dkif.leggtil"
+                lenkeType={'EKSTERN'}
+                ikon={eksternLenkeIkon}
+            />
+        </>
+    );
 };
 
 export default KontaktInformasjon;
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,243 +1,243 @@
-import { FetchKontaktInfo } from "../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF-Fetch";
-import { PersonInfo } from "../types/personInfo";
-import { KontaktInfo } from "../types/kontaktInfo";
-import { HTTPError } from "../components/error/Error";
-import { FetchFeatureToggles } from "./providers/FeatureToggles";
-import { FetchDsopInfo } from "../pages/digital-samhandling-offentlig-privat/DsopFetch";
-import { DsopInfo } from "../types/dsop";
-import { FetchPersonInfo } from "./providers/PersonInfo";
-import { InstInfo } from "../types/inst";
-import { FetchInstInfo } from "../pages/institusjonsopphold/InstFetch";
-import { FetchMedlInfo } from "../pages/medlemskap-i-folketrygden/MedlFetch";
-import { MedlInfo } from "../types/medl";
-import { Auth, FetchAuth } from "../types/authInfo";
+import { FetchKontaktInfo } from '../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/kontakt-og-reservasjonsregisteret/DKIF-Fetch';
+import { PersonInfo } from '../types/personInfo';
+import { KontaktInfo } from '../types/kontaktInfo';
+import { HTTPError } from '../components/error/Error';
+import { FetchFeatureToggles } from './providers/FeatureToggles';
+import { FetchDsopInfo } from '../pages/digital-samhandling-offentlig-privat/DsopFetch';
+import { DsopInfo } from '../types/dsop';
+import { FetchPersonInfo } from './providers/PersonInfo';
+import { InstInfo } from '../types/inst';
+import { FetchInstInfo } from '../pages/institusjonsopphold/InstFetch';
+import { FetchMedlInfo } from '../pages/medlemskap-i-folketrygden/MedlFetch';
+import { MedlInfo } from '../types/medl';
+import { Auth, FetchAuth } from '../types/authInfo';
 
 export interface FeatureToggles {
-  [key: string]: boolean;
+    [key: string]: boolean;
 }
 
-const initialLocale = ((window.location.pathname.match(/\/en($|\/)/) && "en") ||
-  (window.location.pathname.match(/\/nn($|\/)/) && "nn") ||
-  "nb") as Locale;
+const initialLocale = ((window.location.pathname.match(/\/en($|\/)/) && 'en') ||
+    (window.location.pathname.match(/\/nn($|\/)/) && 'nn') ||
+    'nb') as Locale;
 
 export const initialState = {
-  formKey: 0,
-  locale: initialLocale,
-  authInfo: { status: "LOADING" } as FetchAuth,
-  featureToggles: {
-    status: "LOADING",
-    data: {
-      "personopplysninger.pdl": false,
-      "personopplysninger.dsop": false,
-      "personopplysninger.inst": false,
-      "personopplysninger.skatt": false,
-      "personopplysninger.medl": false,
-      "personopplysninger.fullmakt": false,
-      "pdl-fullmakt": false,
-    },
-  } as FetchFeatureToggles,
-  dsopInfo: { status: "LOADING" } as FetchDsopInfo,
-  instInfo: { status: "LOADING" } as FetchInstInfo,
-  personInfo: { status: "LOADING" } as FetchPersonInfo,
-  kontaktInfo: { status: "LOADING" } as FetchKontaktInfo,
-  medlInfo: { status: "LOADING" } as FetchMedlInfo,
+    formKey: 0,
+    locale: initialLocale,
+    authInfo: { status: 'LOADING' } as FetchAuth,
+    featureToggles: {
+        status: 'LOADING',
+        data: {
+            'personopplysninger.pdl': false,
+            'personopplysninger.dsop': false,
+            'personopplysninger.inst': false,
+            'personopplysninger.skatt': false,
+            'personopplysninger.medl': false,
+            'personopplysninger.fullmakt': false,
+            'pdl-fullmakt': false,
+        },
+    } as FetchFeatureToggles,
+    dsopInfo: { status: 'LOADING' } as FetchDsopInfo,
+    instInfo: { status: 'LOADING' } as FetchInstInfo,
+    personInfo: { status: 'LOADING' } as FetchPersonInfo,
+    kontaktInfo: { status: 'LOADING' } as FetchKontaktInfo,
+    medlInfo: { status: 'LOADING' } as FetchMedlInfo,
 };
-export type Locale = "nb" | "en" | "nn";
+export type Locale = 'nb' | 'en' | 'nn';
 
 export interface Store {
-  formKey: number;
-  locale: Locale;
-  authInfo: FetchAuth;
-  featureToggles: FetchFeatureToggles;
-  personInfo: FetchPersonInfo;
-  dsopInfo: FetchDsopInfo;
-  instInfo: FetchInstInfo;
-  kontaktInfo: FetchKontaktInfo;
-  medlInfo: FetchMedlInfo;
+    formKey: number;
+    locale: Locale;
+    authInfo: FetchAuth;
+    featureToggles: FetchFeatureToggles;
+    personInfo: FetchPersonInfo;
+    dsopInfo: FetchDsopInfo;
+    instInfo: FetchInstInfo;
+    kontaktInfo: FetchKontaktInfo;
+    medlInfo: FetchMedlInfo;
 }
 
 export type Action =
-  | {
-      type: "SETT_LOCALE";
-      payload: Locale;
-    }
-  | {
-      type: "SETT_AUTH_RESULT";
-      payload: Auth;
-    }
-  | {
-      type: "SETT_AUTH_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_FEATURE_TOGGLES";
-      payload: FeatureToggles;
-    }
-  | {
-      type: "SETT_PERSON_INFO_RESULT";
-      payload: PersonInfo;
-    }
-  | {
-      type: "SETT_PERSON_INFO_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_KONTAKT_INFO_RESULT";
-      payload: KontaktInfo;
-    }
-  | {
-      type: "SETT_KONTAKT_INFO_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_DSOP_INFO_RESULT";
-      payload: DsopInfo;
-    }
-  | {
-      type: "SETT_DSOP_INFO_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_INST_INFO_RESULT";
-      payload: InstInfo;
-    }
-  | {
-      type: "SETT_INST_INFO_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_SKATT_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "SETT_MEDL_INFO_RESULT";
-      payload: MedlInfo;
-    }
-  | {
-      type: "SETT_MEDL_INFO_ERROR";
-      payload: HTTPError;
-    }
-  | {
-      type: "INCREASE_FORM_KEY";
-    };
+    | {
+          type: 'SETT_LOCALE';
+          payload: Locale;
+      }
+    | {
+          type: 'SETT_AUTH_RESULT';
+          payload: Auth;
+      }
+    | {
+          type: 'SETT_AUTH_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_FEATURE_TOGGLES';
+          payload: FeatureToggles;
+      }
+    | {
+          type: 'SETT_PERSON_INFO_RESULT';
+          payload: PersonInfo;
+      }
+    | {
+          type: 'SETT_PERSON_INFO_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_KONTAKT_INFO_RESULT';
+          payload: KontaktInfo;
+      }
+    | {
+          type: 'SETT_KONTAKT_INFO_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_DSOP_INFO_RESULT';
+          payload: DsopInfo;
+      }
+    | {
+          type: 'SETT_DSOP_INFO_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_INST_INFO_RESULT';
+          payload: InstInfo;
+      }
+    | {
+          type: 'SETT_INST_INFO_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_SKATT_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'SETT_MEDL_INFO_RESULT';
+          payload: MedlInfo;
+      }
+    | {
+          type: 'SETT_MEDL_INFO_ERROR';
+          payload: HTTPError;
+      }
+    | {
+          type: 'INCREASE_FORM_KEY';
+      };
 
 export const reducer = (state: Store, action: Action) => {
-  switch (action.type) {
-    case "SETT_LOCALE":
-      return {
-        ...state,
-        locale: action.payload,
-      };
-    case "SETT_AUTH_RESULT":
-      return {
-        ...state,
-        authInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchAuth,
-      };
-    case "SETT_AUTH_ERROR":
-      return {
-        ...state,
-        authInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchAuth,
-      };
-    case "SETT_FEATURE_TOGGLES":
-      return {
-        ...state,
-        featureToggles: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchFeatureToggles,
-      };
-    case "SETT_PERSON_INFO_RESULT":
-      return {
-        ...state,
-        personInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchPersonInfo,
-      };
-    case "SETT_PERSON_INFO_ERROR":
-      return {
-        ...state,
-        personInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchPersonInfo,
-      };
-    case "SETT_KONTAKT_INFO_RESULT":
-      return {
-        ...state,
-        kontaktInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchKontaktInfo,
-      };
-    case "SETT_KONTAKT_INFO_ERROR":
-      return {
-        ...state,
-        kontaktInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchKontaktInfo,
-      };
-    case "SETT_DSOP_INFO_RESULT":
-      return {
-        ...state,
-        dsopInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchDsopInfo,
-      };
-    case "SETT_DSOP_INFO_ERROR":
-      return {
-        ...state,
-        dsopInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchDsopInfo,
-      };
-    case "SETT_INST_INFO_RESULT":
-      return {
-        ...state,
-        instInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchInstInfo,
-      };
-    case "SETT_INST_INFO_ERROR":
-      return {
-        ...state,
-        instInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchInstInfo,
-      };
-    case "SETT_MEDL_INFO_RESULT":
-      return {
-        ...state,
-        medlInfo: {
-          status: "RESULT",
-          data: action.payload,
-        } as FetchMedlInfo,
-      };
-    case "SETT_MEDL_INFO_ERROR":
-      return {
-        ...state,
-        medlInfo: {
-          status: "ERROR",
-          error: action.payload,
-        } as FetchMedlInfo,
-      };
-    case "INCREASE_FORM_KEY":
-      return {
-        ...state,
-        formKey: state.formKey + 1,
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case 'SETT_LOCALE':
+            return {
+                ...state,
+                locale: action.payload,
+            };
+        case 'SETT_AUTH_RESULT':
+            return {
+                ...state,
+                authInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchAuth,
+            };
+        case 'SETT_AUTH_ERROR':
+            return {
+                ...state,
+                authInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchAuth,
+            };
+        case 'SETT_FEATURE_TOGGLES':
+            return {
+                ...state,
+                featureToggles: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchFeatureToggles,
+            };
+        case 'SETT_PERSON_INFO_RESULT':
+            return {
+                ...state,
+                personInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchPersonInfo,
+            };
+        case 'SETT_PERSON_INFO_ERROR':
+            return {
+                ...state,
+                personInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchPersonInfo,
+            };
+        case 'SETT_KONTAKT_INFO_RESULT':
+            return {
+                ...state,
+                kontaktInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchKontaktInfo,
+            };
+        case 'SETT_KONTAKT_INFO_ERROR':
+            return {
+                ...state,
+                kontaktInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchKontaktInfo,
+            };
+        case 'SETT_DSOP_INFO_RESULT':
+            return {
+                ...state,
+                dsopInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchDsopInfo,
+            };
+        case 'SETT_DSOP_INFO_ERROR':
+            return {
+                ...state,
+                dsopInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchDsopInfo,
+            };
+        case 'SETT_INST_INFO_RESULT':
+            return {
+                ...state,
+                instInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchInstInfo,
+            };
+        case 'SETT_INST_INFO_ERROR':
+            return {
+                ...state,
+                instInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchInstInfo,
+            };
+        case 'SETT_MEDL_INFO_RESULT':
+            return {
+                ...state,
+                medlInfo: {
+                    status: 'RESULT',
+                    data: action.payload,
+                } as FetchMedlInfo,
+            };
+        case 'SETT_MEDL_INFO_ERROR':
+            return {
+                ...state,
+                medlInfo: {
+                    status: 'ERROR',
+                    error: action.payload,
+                } as FetchMedlInfo,
+            };
+        case 'INCREASE_FORM_KEY':
+            return {
+                ...state,
+                formKey: state.formKey + 1,
+            };
+        default:
+            return state;
+    }
 };
