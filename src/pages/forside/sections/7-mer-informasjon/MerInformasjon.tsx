15ea67cdc8cb89cf2725f1be6369bcf46a49f590
Bjørn Harbo Dahle <bdahle@hey.com>
2023-11-13 16:28:40 +0100

    bytt Panel med Box

--- src/pages/forside/sections/7-mer-informasjon/MerInformasjon.tsx
+++ src/pages/forside/sections/7-mer-informasjon/MerInformasjon.tsx
@@ -2,13 +2,13 @@ import { Fragment } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import MerInformasjonPanelinnhold from './MerInformasjonPanelinnhold';
 import merInformasjon from './MerInformasjonData';
-import { Heading, Panel, ReadMore } from '@navikt/ds-react';
+import { Box, Heading, ReadMore } from '@navikt/ds-react';
 
 const AlternativListe = () => {
     const { formatMessage: msg } = useIntl();
 
     return (
-        <Panel className="el__panel">
+        <Box background="surface-default" className="el__panel">
             <div className="mi__content">
                 <Heading size={'medium'} level={'2'}>
                     <FormattedMessage id="alternativer.tittel" />
@@ -21,7 +21,7 @@ const AlternativListe = () => {
                     </Fragment>
                 ))}
             </div>
-        </Panel>
+        </Box>
     );
 };
 
