2576d8acf1821ec1899304a8074d70f644c35621
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-12-15 13:11:59 +0100

    Oppdaterer actions. Tar inn endringer relatert til autentisering ved endring av kontonummer.

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,9 +1,14 @@
 import { logApiError } from '../utils/logger';
-import { OutboundTlfnummer } from '../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/telefonnummer/Telefonnummer';
+import {
+    OutboundTlfnummer,
+} from '../pages/forside/sections/4-personinfo/2-kontaktinfo/subsections/telefonnummer/Telefonnummer';
 import { TPSResponse } from '../types/tps-response';
 import { Feilmelding } from '../components/httpFeilmelding/HttpFeilmelding';
 import { getLoginRedirectUrl } from '../utils/redirects';
-import { OutboundNorskKontonummer, OutboundUtenlandsbankonto } from '../pages/forside/sections/4-personinfo/4-utbetalinger/endring/types';
+import {
+    OutboundNorskKontonummer,
+    OutboundUtenlandsbankonto,
+} from '../pages/forside/sections/4-personinfo/4-utbetalinger/endring/types';
 
 const parseJson = (data: Response) => data.json();
 
@@ -81,18 +86,55 @@ const postJson = (url: string, data?: Outbound) => {
         });
 };
 
+const reauthenticate = (url: string, data?: Outbound) => {
+    return fetch(url, {
+        method: 'POST',
+        ...(data && {
+            body: JSON.stringify(data),
+        }),
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        credentials: 'include',
+    })
+        .then(sjekkHttpFeil)
+        .then(sjekkRedirect)
+        .catch((err: string & Feilmelding) => {
+            const error = {
+                code: err.code || 404,
+                type: err.type || 'feil',
+                text: err.text ?? err,
+            };
+            logApiError(url, error);
+            throw error;
+        });
+};
+
 export const postTlfnummer = (data: OutboundTlfnummer) => postJson(`${REACT_APP_API_URL}/endreTelefonnummer`, data);
 
 export const slettTlfnummer = (data: OutboundTlfnummer) => postJson(`${REACT_APP_API_URL}/slettTelefonnummer`, data);
 
 export const postKontonummer = (data: OutboundNorskKontonummer | OutboundUtenlandsbankonto) =>
-    postJson(`${REACT_APP_API_URL}/endreKontonummer`, data);
+    reauthenticate(`${REACT_APP_API_URL}/endreKontonummer`, data);
 
 export const slettKontaktadresse = () => postJson(`${REACT_APP_API_URL}/slettKontaktadresse`);
 
 /*
     UTILS
  */
+
+const sjekkRedirect = async (response: Response) => {
+    if (response.ok) {
+        const data = response.json();
+        data.then((data) => {
+            window.location.assign(data.redirect || '');
+        });
+        return response;
+    }
+
+    throw {
+        code: response.status,
+        text: response.status === 400 ? await response.text() : 'Oisann, noe gikk galt! Prøv igjen senere.',
+    };
+};
 
 const sjekkAuth = (response: Response): Response => {
     if (response.status === 401 || response.status === 403) {
@@ -135,8 +177,8 @@ const sjekkTPSFeil = (response: TPSResponse) => {
                 text: `${response.error && response.error.message}${
                     response.error && response.error.details
                         ? `\n${Object.values(response.error.details)
-                              .map((details) => details.join('\n'))
-                              .join('\n')}`
+                            .map((details) => details.join('\n'))
+                            .join('\n')}`
                         : ''
                 }`,
             },
