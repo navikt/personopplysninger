9e9f754c141a42e37b3e0cdde36ef9c04572e35f
Terje Karlsen <terjeofnorway@gmail.com>
2024-05-07 14:40:36 +0200

    Justerer params for utloggingsvarsel

--- server/dekorator.js
+++ server/dekorator.js
@@ -4,17 +4,19 @@ const getHtmlWithDecorator = (filePath) =>
     injectDecoratorServerSide({
         env: process.env.ENV,
         filePath: filePath,
-        enforceLogin: false,
-        breadcrumbs: [
-            { url: `https://www.nav.no/minside/`, title: 'Min side' },
-            { url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger` },
-        ],
-        availableLanguages: [
-            { url: `https://www.nav.no/person/personopplysninger/nb/`, locale: 'nb' },
-            { url: `https://www.nav.no/person/personopplysninger/en/`, locale: 'en' },
-            { url: `https://www.nav.no/person/personopplysninger/nn/`, locale: 'nn' },
-        ],
-        logoutWarning: true,
+        params: {
+            enforceLogin: false,
+            breadcrumbs: [
+                { url: `https://www.nav.no/minside/`, title: 'Min side' },
+                { url: `https://www.nav.no/person/personopplysninger/`, title: `Personopplysninger` },
+            ],
+            availableLanguages: [
+                { url: `https://www.nav.no/person/personopplysninger/nb/`, locale: 'nb' },
+                { url: `https://www.nav.no/person/personopplysninger/en/`, locale: 'en' },
+                { url: `https://www.nav.no/person/personopplysninger/nn/`, locale: 'nn' },
+            ],
+            logoutWarning: true,
+        },
     });
 
 module.exports = getHtmlWithDecorator;
