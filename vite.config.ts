b7840d9afdf9a9994ecbd7de5bde9111cac8e88e
Terje Karlsen <terjeofnorway@gmail.com>
2024-07-04 14:13:42 +0200

    Fjerner cust om __dirname i vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
 import path from 'path';
-
-// Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
     return {
